ae6afb3b747a4b907555063aec7cfa685e6040dc
Matej Kminek <matej.kminek@attendees.eu>
2017-05-10 19:26:42 +0200

    Created TestAuthenticator for testing purposes

---
+++ tests/TestAuthenticator.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Model;
+
+use Nette;
+
+/**
+ * Users management.
+ */
+class TestAuthenticator implements Nette\Security\IAuthenticator {
+
+    private $id;
+    private $status;
+    private $arr;
+    
+    /**
+     * Performs an authentication.
+     * @return Nette\Security\Identity
+     * @throws Nette\Security\AuthenticationException
+     */
+    public function authenticate(array $credentials) {
+        return new Nette\Security\Identity($this->id, $this->status, $this->arr );
+    }
+    
+    public function setId($id){
+        $this->id = $id;
+    }
+    
+    public function setStatus($status){
+        $this->status = $status;
+    }
+    
+    public function setArr($arr){
+        $this->arr = $arr;
+    }
+}
