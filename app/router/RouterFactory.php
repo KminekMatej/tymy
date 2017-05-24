a5129e6389cf84823651fe4e2939b58fec074e4f
Matej Kminek <matej.kminek@attendees.eu>
2017-05-24 13:39:07 +0200

    Routes for player types

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -14,13 +14,15 @@ class RouterFactory
 	/**
 	 * @return Nette\Application\IRouter
 	 */
-	public static function createRouter()
-	{
+	public static function createRouter(){
 		$router = new RouteList;
                 $router[] = new Route('index.php', 'Homepage:default', Route::ONE_WAY);
                 $router[] = new Route('diskuze', 'Discussion:default');
                 $router[] = new Route('udalosti', 'Event:default');
                 $router[] = new Route('tym', 'Team:default');
+                $router[] = new Route('tym/hraci', 'Team:players');
+                $router[] = new Route('tym/clenove', 'Team:members');
+                $router[] = new Route('tym/marodi', 'Team:sicks');
                 $router[] = new Route('ankety', 'Poll:default');
                 $router[] = new Route('diskuze/<discussion>[/<page>] ? search=<search>', 'Discussion:discussion');
                 $router[] = new Route('udalosti/<udalost>', 'Event:event');
