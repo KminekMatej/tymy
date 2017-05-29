24c6d2937f681606cba062fe163673426826919c
Matej Kminek <matej.kminek@attendees.eu>
2017-05-29 11:53:31 +0200

    addPost function moved to javascript file

--- www/js/discussion.js
+++ www/js/discussion.js
@@ -1,6 +1,15 @@
-function copyPost(elm){
-    var text = $("#"+elm).html();
-    clipboard.copy({"text/html":text});
+
+function addPost(url) {
+    $.nette.ajax({
+        type: 'POST',
+        url: url,
+        data: {'post': CKEDITOR.instances.addPost.getData()},
+    });
+}
+
+function copyPost(elm) {
+    var text = $("#" + elm).html();
+    clipboard.copy({"text/html": text});
 }
 
 function loadPost(elm) {
