0869353cbde90c38f9680a1499bf2cb7d33f920d
Matej Kminek <matej.kminek@attendees.eu>
2017-06-01 18:50:47 +0200

    IP edits

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
-$configurator->setDebugMode('94.112.206.90');
+$configurator->setDebugMode(['94.112.206.90','90.182.181.138']);
 $configurator->enableTracy(__DIR__ . '/../log');
 
 $configurator->setTimeZone('Europe/Prague');
