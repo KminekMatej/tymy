5787f3bc215920a91fd681f5c952f851ca972ba6
Matej Kminek <matej.kminek@attendees.eu>
2017-06-03 10:52:19 +0200

    Test parameters are obtained from SETUP file. Obligatory to make possible of multiple setups

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,9 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$GLOBALS["username"] = "autotest";
-$GLOBALS["password"] = "123456.";
-$GLOBALS["invasive"] = FALSE;
+$GLOBALS["username"] = $runner->testedTeam["user"];
+$GLOBALS["password"] = $runner->testedTeam["pass"];
+$GLOBALS["invasive"] = $runner->testedTeam["invasive"];
 $GLOBALS["dateRegex"] = '/^(?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d(?:Z|[+-][01]\d:[0-5]\d)$/m';
 
 Tester\Environment::setup();
@@ -19,4 +19,5 @@ $configurator->createRobotLoader()
 
 $configurator->addConfig(__DIR__ . '/../app/config/config.neon');
 $configurator->addConfig(__DIR__ . '/../app/config/config.local.neon');
+$configurator->addParameters(['tym' => $runner->testedTeam["team"]]);
 return $configurator->createContainer();
