9a6395b6d92cd47c16ba9c2f9dbadadc0faf0e54
Matej Kminek <matej.kminek@attendees.eu>
2017-06-06 12:25:15 +0200

    Votes are postprocessed only when exists. Anonymous results cannot have myVotes

--- app/model/tymy/Poll.php
+++ app/model/tymy/Poll.php
@@ -43,11 +43,12 @@ final class Poll extends Tymy{
         $data->webName = \Nette\Utils\Strings::webalize($data->caption);
         $this->timezone($data->createdAt);
         $this->timezone($data->updatedAt);
-        foreach ($data->votes as $vote) {
-            if($vote->userId == $this->user->getId()){
-                $data->myVotes[$vote->optionId] = $vote;
+        if (property_exists($data, "votes"))
+            foreach ($data->votes as $vote) {
+                if (!$data->anonymousResults && $vote->userId == $this->user->getId()) {
+                    $data->myVotes[$vote->optionId] = $vote;
+                }
+                $this->timezone($vote->updatedAt);
             }
-            $this->timezone($vote->updatedAt);
-        }
     }
 }
