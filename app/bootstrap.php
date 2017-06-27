0065e935393e2ec1928cf8c5f5cb49c4b15dc1ac
Matej Kminek <matej.kminek@attendees.eu>
2017-06-27 10:02:05 +0200

    Tym specification moved to config.local.neon

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -17,9 +17,6 @@ $configurator->createRobotLoader()
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . '/config/config.local.neon');
 
-//TEAM DEFINED CONFIGURATIONS
-$configurator->addParameters(['tym' => "dev"]);
-
 $container = $configurator->createContainer();
 
 return $container;
