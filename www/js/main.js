b11b490a2434b0f50941d0861b11484d34eedb32
Matej Kminek <matej.kminek@attendees.eu>
2017-06-28 09:30:52 +0200

    JS corrections

--- www/js/main.js
+++ www/js/main.js
@@ -5,11 +5,11 @@ $(document).ready(function () {
 function reTitle() {
     numTitle = $("NAV[data-title]").attr("data-title");
     cnt = (document.title.match(/\|/g) || []).length;
-    if (cnt == 1)
+    if (cnt == 1){
         if(numTitle > 0)
             document.title = numTitle + " | " + document.title;
-    else if (numTitle === "0")
+    } else if (numTitle === "0") {
         document.title = document.title.replace(new RegExp(/\d+ \| /), "");
-    else
+    } else
         document.title = document.title.replace(new RegExp(/\d+/), numTitle);
 }
