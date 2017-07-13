7ed58405bcd49642d5aadd00a02e3c8eb0b58b65
Matej Kminek <matej.kminek@attendees.eu>
2017-07-13 21:53:44 +0200

    Modernizations for serving as proper head

--- tests/tapi/ITapiTest.php
+++ tests/tapi/ITapiTest.php
@@ -8,7 +8,7 @@ use Tester;
  *
  * @author kminekmatej
  */
-class ITapiTest extends Tester\TestCase {
+abstract class ITapiTest extends Tester\TestCase {
     
     /** @var \App\Model\Supplier */
     protected $supplier;
@@ -21,12 +21,13 @@ class ITapiTest extends Tester\TestCase {
     protected $tapiAuthenticator;
     /** @var \App\Model\TestAuthenticator */
     protected $testAuthenticator;
+    /** @var \Nette\Security\User */
+    protected $user;
     
-    public function __construct(\App\Model\Supplier $supplier) {
-        $this->supplier = $supplier;
-    }
     
     protected function setUp(){
+        $this->supplier = $this->container->getByType('App\Model\Supplier');
+        $this->user = $this->container->getByType('Nette\Security\User');
         $tapi_config = $this->supplier->getTapi_config();
         $tapi_config["tym"] = $GLOBALS["testedTeam"]["team"];
         $this->tapi_config = $tapi_config;
@@ -34,8 +35,27 @@ class ITapiTest extends Tester\TestCase {
         $this->supplier->setTapi_config($tapi_config);
         $this->tapiAuthenticator = new \App\Model\TapiAuthenticator($this->supplier);
         $this->testAuthenticator = new \App\Model\TestAuthenticator();
+        $tested_object = $this->getTestedObject();
+        $this->objectExists($tested_object);
+        $tested_object->setSupplier($this->supplier);
     }
     
+    protected function userTapiAuthenticate($username, $password){
+        $this->user->setAuthenticator($this->tapiAuthenticator);
+        $this->user->login($username, $password);
+    }
+    
+    protected function userTestAuthenticate($username, $password){
+        $this->user->setAuthenticator($this->testAuthenticator);
+        $this->user->login($username, $password);
+    }
+    
+    protected function objectExists($object){
+        \Tester\Assert::truthy($object);
+    }
+
+    abstract function getTestedObject();
+
     public function login(){
         $this->loginObj = new \Tymy\Login();
         $this->login = $this->loginObj->setSupplier($this->supplier)
