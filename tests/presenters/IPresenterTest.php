3d8b4c01cdbe405b3d492893839fba0b65054b99
Matej Kminek <matej.kminek@attendees.eu>
2017-07-20 09:41:47 +0200

    Introduces interface for presenter testing, Homepage presenter test modernized

---
+++ tests/presenters/IPresenterTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Test;
+
+use Nette\Application\Request;
+use Tester\Assert;
+
+/**
+ *
+ * @author kminekmatej
+ */
+abstract class IPresenterTest extends \Tester\TestCase {
+    
+    /** @var \App\Model\Supplier */
+    protected $supplier;
+    
+    /** @var \App\Model\TapiAuthenticator */
+    protected $tapiAuthenticator;
+    /** @var \App\Model\TestAuthenticator */
+    protected $testAuthenticator;
+    /** @var \Nette\Security\User */
+    protected $user;
+    
+    /** @var \App\Presenters\SecuredPresenter */
+    protected $presenter;
+    
+    protected $container;
+    
+    protected function setUp() {
+        $this->supplier = $this->container->getByType('App\Model\Supplier');
+        $this->user = $this->container->getByType('Nette\Security\User');
+        $tapi_config = $this->supplier->getTapi_config();
+        $tapi_config["tym"] = $GLOBALS["testedTeam"]["team"];
+        $tapi_config["root"] = $GLOBALS["testedTeam"]["root"];
+        
+        $this->supplier->setTapi_config($tapi_config);
+        $this->tapiAuthenticator = new \App\Model\TapiAuthenticator($this->supplier);
+        $this->testAuthenticator = new \App\Model\TestAuthenticator($this->supplier);
+        
+        $presenterFactory = $this->container->getByType('Nette\Application\IPresenterFactory');
+        $this->presenter = $presenterFactory->createPresenter($this->getPresenterName());
+        $this->presenter->autoCanonicalize = FALSE;
+
+        return parent::setUp();
+    }
+
+    protected function tearDown() {
+        return parent::tearDown();
+    }
+    
+    private function getPresenterName(){
+        $class = get_called_class();
+        return $class::PRESENTERNAME;
+    }
+    
+    protected function userTapiAuthenticate($username, $password){
+        $this->user->setAuthenticator($this->tapiAuthenticator);
+        $this->user->login($username, $password);
+    }
+    
+    protected function userTestAuthenticate($username, $password){
+        $this->user->setAuthenticator($this->testAuthenticator);
+        $this->user->login($username, $password);
+    }
+    
+    function testSignInFailsRedirects(){
+        $this->user->logout();
+        $request = new Request($this->getPresenterName(), 'GET', array('action' => 'default'));
+        $response = $this->presenter->run($request);
+
+        Assert::type('Nette\Application\Responses\RedirectResponse', $response);
+        Assert::equal('http:///sign/in', substr($response->getUrl(), 0, 15));
+        Assert::equal(302, $response->getCode());
+    }
+
+    
+}
