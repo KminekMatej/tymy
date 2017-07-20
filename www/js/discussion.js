e58c0e866747a07218f00fcf07f6473ebf96c20e
Matej Kminek <matej.kminek@attendees.eu>
2017-07-20 20:38:43 +0200

    Editing post

--- www/js/discussion.js
+++ www/js/discussion.js
@@ -1,12 +1,12 @@
 
 function addPost(url) {
-    $("DIV.addPost BTN.btn-primary").prop("disabled", true);
+    $("DIV.addPost BUTTON").prop("disabled", true);
     $.nette.ajax({
         type: 'POST',
         url: url,
         data: {'post': CKEDITOR.instances.addPost.getData()},
     }).done(function () {
-        $("DIV.addPost BTN.btn-primary").prop("disabled", false);
+        $("DIV.addPost BUTTON").prop("disabled", false);
         CKEDITOR.instances.addPost.setData('');
     });
 }
@@ -16,8 +16,25 @@ function copyPost(elm) {
     clipboard.copy({"text/html": text});
 }
 
-function loadPost(elm) {
-    var text = $("#" + elm).html();
+function loadPost(postId) {
+    var text = $("#content_" + postId).html();
+    $("DIV.addPost").attr("data-postId",postId);
     CKEDITOR.instances.addPost.setData(text);
     smoothScroll('addPost');
+}
+
+function updatePost(url) {
+    $("DIV.addPost BUTTON").prop("disabled", true);
+    $.nette.ajax({
+        type: 'POST',
+        url: url,
+        data: {
+            'postId': $("DIV.addPost").attr("data-postId"),
+            'post': CKEDITOR.instances.addPost.getData(),
+        },
+    }).done(function () {
+        $("DIV.addPost BUTTON").prop("disabled", false);
+        $("DIV.addPost").attr("data-postId","");
+        CKEDITOR.instances.addPost.setData('');
+    });
 }
