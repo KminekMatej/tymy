5e839c7607ec386cb807ac05d720dcdd0cc5f6a5
Matej Kminek <matej.kminek@attendees.eu>
2017-07-25 21:54:18 +0200

    Timer now refreshes except navbar also homepage, if is currently active

--- www/js/timer.js
+++ www/js/timer.js
@@ -2,14 +2,33 @@ $(function () {
     $.nette.init();
     
     setTimeout(function(){nav();}, 5000);
+});
 
-    function nav(){
-        $.nette.ajax({
-            url: "/?do=navbar-refresh",
-            complete: function (payload) {
-                reTitle();
-                setTimeout(function(){nav();}, 5000);
+function nav() {
+    $.nette.ajax({
+        url: "/?do=navbar-refresh",
+        complete: function (payload) {
+            reTitle();
+            setTimeout(function () {
+                nav();
+            }, 5000);
+            homepageRefresh();
+        }
+    });
+}
+
+function homepageRefresh() {
+    if ($("DIV.container.homepage")) {
+        $("NAV UL.navbar-nav LI.nav-item.dropdown:first-child A.dropdown-item").each(function () {
+            count = $(this).find("SPAN.badge").html();
+            id = $(this).attr("data-id")
+            $("DIV.container.homepage DIV.name#" + id + " SPAN.badge").html(count);
+            if (count > 0) {
+                $("DIV.container.homepage DIV.name#" + id).removeClass("hidden-badge");
+            } else {
+                $("DIV.container.homepage DIV.name#" + id).addClass("hidden-badge");
             }
+
         });
     }
-});
+}
