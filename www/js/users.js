43736ed8cbc24f5008bf615da43933ff57b8ceee
Matej Kminek <matej.kminek@attendees.eu>
2017-07-26 10:43:42 +0200

    Phone number can be erased

--- www/js/users.js
+++ www/js/users.js
@@ -85,6 +85,7 @@ function isValid(name, value1, value2 = null){
             var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             return re.test(value1);
         case "phone":
+            if(value1=="") return true;
             var re = /^[+]?[()/0-9. -]{9,}$/;
             return re.test(value1);
         case "login":
