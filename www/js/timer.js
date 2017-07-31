4c4b3f272f84b939b1770c611404f050a3a99f71
Matej Kminek <matej.kminek@attendees.eu>
2017-07-31 07:53:31 +0200

    Fix homepage discussion refreshing

--- www/js/timer.js
+++ www/js/timer.js
@@ -21,12 +21,12 @@ function homepageRefresh() {
     if ($("DIV.container.homepage")) {
         $("NAV UL.navbar-nav LI.nav-item.dropdown:first-child A.dropdown-item").each(function () {
             count = $(this).find("SPAN.badge").html();
-            id = $(this).attr("data-id")
-            $("DIV.container.homepage DIV.name#" + id + " SPAN.badge").html(count);
+            id = $(this).attr("id");
+            $("DIV.container.homepage DIV#" + id + ".name SPAN.badge").html(count);
             if (count > 0) {
-                $("DIV.container.homepage DIV.name#" + id).removeClass("hidden-badge");
+                $("DIV.container.homepage DIV#" + id + ".name").removeClass("hidden-badge");
             } else {
-                $("DIV.container.homepage DIV.name#" + id).addClass("hidden-badge");
+                $("DIV.container.homepage DIV#" + id + ".name").addClass("hidden-badge");
             }
 
         });
