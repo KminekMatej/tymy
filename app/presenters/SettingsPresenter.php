8e5eb6a8049aad7bb890b08b1b18fa924a7abf67
Matej Kminek <matej.kminek@attendees.eu>
2017-08-16 09:54:26 +0200

    Setting details handled by actions

--- app/presenters/SettingsPresenter.php
+++ app/presenters/SettingsPresenter.php
@@ -3,6 +3,9 @@
 namespace App\Presenters;
 
 class SettingsPresenter extends SecuredPresenter {
+        
+    /** @var \Tymy\Event @inject */
+    public $event;
     
     protected function startup() {
         parent::startup();
@@ -10,38 +13,75 @@ class SettingsPresenter extends SecuredPresenter {
     }
 
     
-    public function renderDefault() {
+    public function actionDefault() {
         //TODO
     }
 
-    public function renderDiscussions() {
-        //TODO
+    public function actionDiscussions($discussion = NULL) {
         $this->setLevelCaptions(["2" => ["caption" => "Diskuze", "link" => $this->link("Settings:discussions")]]);
+        if(!is_null($discussion)){
+            $this->setView("discussion");
+        } else {
+            //TODO render list
+        }
     }
 
-    public function renderEvents() {
-        //TODO
+    public function actionEvents($event = NULL) {
         $this->setLevelCaptions(["2" => ["caption" => "Události", "link" => $this->link("Settings:events")]]);
+        if(!is_null($event)){
+            $this->setView("event");
+        } else {
+            //TODO render list
+        }
     }
 
-    public function renderPolls() {
-        //TODO
+    public function actionPolls($poll = NULL) {
         $this->setLevelCaptions(["2" => ["caption" => "Ankety", "link" => $this->link("Settings:polls")]]);
+        if(!is_null($poll)){
+            $this->setView("poll");
+        } else {
+            //TODO render list
+        }
     }
 
-    public function renderReports() {
+    public function actionReports() {
         //TODO
         $this->setLevelCaptions(["2" => ["caption" => "Reporty", "link" => $this->link("Settings:reports")]]);
     }
 
-    public function renderPermissions() {
+    public function actionPermissions() {
         //TODO
         $this->setLevelCaptions(["2" => ["caption" => "Oprávnění", "link" => $this->link("Settings:permissions")]]);
     }
 
-    public function renderApp() {
+    public function actionApp() {
         //TODO
         $this->setLevelCaptions(["2" => ["caption" => "Aplikace", "link" => $this->link("Settings:app")]]);
     }
-
+    
+    
+    public function renderDiscussion($discussion) {
+        //RENDERING DISCUSSION DETAIL
+        $discussionId = $this->parseIdFromWebname($discussion);
+        $discussionObj = $this->event->reset()->recId($discussionId)->getData();
+        $this->setLevelCaptions(["3" => ["caption" => $discussionObj->caption, "link" => $this->link("Settings:discussions", $discussionObj->webName)]]);
+        $this->template->discussion = $discussionObj;
+    }
+    
+    public function renderEvent($event) {
+        //RENDERING EVENT DETAIL
+        $eventId = $this->parseIdFromWebname($event);
+        $eventObj = $this->event->reset()->recId($eventId)->getData();
+        $this->setLevelCaptions(["3" => ["caption" => $eventObj->caption, "link" => $this->link("Settings:events", $eventObj->webName)]]);
+        $this->template->event = $eventObj;
+    }
+    
+    public function renderPoll($poll) {
+        //RENDERING POLL DETAIL
+        $pollId = $this->parseIdFromWebname($poll);
+        $pollObj = $this->event->reset()->recId($pollId)->getData();
+        $this->setLevelCaptions(["3" => ["caption" => $pollObj->caption, "link" => $this->link("Settings:polls", $pollObj->webName)]]);
+        $this->template->poll = $pollObj;
+    }
+    
 }
