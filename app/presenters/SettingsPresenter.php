323d7034e52e1d7988f5823ca37c25382cbd4ccd
Matej Kminek <matej.kminek@attendees.eu>
2017-08-16 08:17:20 +0200

    Settings level captions

--- app/presenters/SettingsPresenter.php
+++ app/presenters/SettingsPresenter.php
@@ -3,29 +3,45 @@
 namespace App\Presenters;
 
 class SettingsPresenter extends SecuredPresenter {
-        
+    
+    protected function startup() {
+        parent::startup();
+        $this->setLevelCaptions(["1" => ["caption" => "Nastavení", "link" => $this->link("Settings:")]]);
+    }
+
+    
     public function renderDefault() {
         //TODO
     }
 
+    public function renderDiscussions() {
+        //TODO
+        $this->setLevelCaptions(["2" => ["caption" => "Diskuze", "link" => $this->link("Settings:discussions")]]);
+    }
+
     public function renderEvents() {
         //TODO
+        $this->setLevelCaptions(["2" => ["caption" => "Události", "link" => $this->link("Settings:events")]]);
     }
 
     public function renderPolls() {
         //TODO
+        $this->setLevelCaptions(["2" => ["caption" => "Ankety", "link" => $this->link("Settings:polls")]]);
     }
 
     public function renderReports() {
         //TODO
+        $this->setLevelCaptions(["2" => ["caption" => "Reporty", "link" => $this->link("Settings:reports")]]);
     }
 
     public function renderPermissions() {
         //TODO
+        $this->setLevelCaptions(["2" => ["caption" => "Oprávnění", "link" => $this->link("Settings:permissions")]]);
     }
 
     public function renderApp() {
         //TODO
+        $this->setLevelCaptions(["2" => ["caption" => "Aplikace", "link" => $this->link("Settings:app")]]);
     }
 
 }
