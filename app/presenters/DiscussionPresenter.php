c30e31595c931b09f94af5c17cc2dd734c6ba63a
Matej Kminek <matej.kminek@attendees.eu>
2017-08-28 09:43:55 +0200

    Pagination moved to presenter head - will be used more frequently

--- app/presenters/DiscussionPresenter.php
+++ app/presenters/DiscussionPresenter.php
@@ -134,21 +134,5 @@ class DiscussionPresenter extends SecuredPresenter {
         $newpost->redrawControl();
         return $newpost;
     }
-    
-    private function pagination($data, $limit = null, $current = null, $adjacents = null) {
-        $result = array();
-
-        if (isset($data, $limit) === true) {
-            $result = range(1, ceil($data / $limit));
-
-            if (isset($current, $adjacents) === true) {
-                if (($adjacents = floor($adjacents / 2) * 2 + 1) >= 1) {
-                    $result = array_slice($result, max(0, min(count($result) - $adjacents, intval($current) - ceil($adjacents / 2))), $adjacents);
-                }
-            }
-        }
-
-        return $result;
-    }
-
+     
 }
--- app/presenters/SecuredPresenter.php
+++ app/presenters/SecuredPresenter.php
@@ -92,4 +92,20 @@ class SecuredPresenter extends BasePresenter {
         $this->redirect($this->getName() . ":default");
     }
     
+    protected function pagination($data, $limit = null, $current = null, $adjacents = null) {
+        $result = array();
+
+        if (isset($data, $limit) === true) {
+            $result = range(1, ceil($data / $limit));
+
+            if (isset($current, $adjacents) === true) {
+                if (($adjacents = floor($adjacents / 2) * 2 + 1) >= 1) {
+                    $result = array_slice($result, max(0, min(count($result) - $adjacents, intval($current) - ceil($adjacents / 2))), $adjacents);
+                }
+            }
+        }
+
+        return $result;
+    }
+    
 }
