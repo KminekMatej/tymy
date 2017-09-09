557dd7d1226741d0a4483ea4741aa91b9ddb37a5
Matej Kminek <matej.kminek@attendees.eu>
2017-09-09 21:34:27 +0200

    Empty link is valid link Task #41 - Nelze založit událost bez odkazu a pravděpodobně i bez umístění

--- www/js/Settings/Settings.js
+++ www/js/Settings/Settings.js
@@ -146,7 +146,7 @@ function validate(elm, name, value1, value2 = null) {
             break;
         case "link":
             var re = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
-            valid = re.test(value1);
+            valid = value1 == "" || re.test(value1);
             break;
         case "caption":
             valid = value1.trim() != "";
@@ -163,6 +163,7 @@ function validate(elm, name, value1, value2 = null) {
 }
 
 function prependHttp(url) {
+    if(url.trim() == "") return "";
     if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
         url = "http://" + url;
     }
