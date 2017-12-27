2355f6df68b3bb5bb1ae6fa8b3fa36920f313c51
Matej Kminek <matej.kminek@attendees.eu>
2017-12-27 21:44:46 +0100

    EventTypeListService introduced

---
+++ app/services/TapiResources/Event/Load/EventTypeListResource.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tapi;
+
+/**
+ * Project: tymy_v2
+ * Description of EventTypeListResource
+ *
+ * @author kminekmatej created on 27.12.2017, 16:46:45
+ */
+class EventTypeListResource extends EventResource {
+    
+    public function init() {
+        $this->setCacheable(CacheService::TIMEOUT_LARGE);
+    }
+    
+    protected function preProcess() {
+        $this->setUrl("eventTypes");
+    }
+    
+    protected function postProcess() {
+        $newData = [];
+        foreach ($this->data as $value) {
+            $newPreStatus = [];
+            foreach ($value->preStatusSet as $status) {
+                $newPreStatus[$status->code] = $status;
+            }
+            $value->preStatusSet = $newPreStatus;
+            $newPostStatus = [];
+            foreach ($value->postStatusSet as $status) {
+                $newPostStatus[$status->code] = $status;
+            }
+            $value->postStatusSet = $newPostStatus;
+            $newData[$value->code] = $value;
+        }
+        $this->data = $newData;
+    }
+    
+}
