47ce55f5129611f02b43e3d000c33c13b77e9e1b
Matej Kminek <matej.kminek@attendees.eu>
2017-12-29 08:53:02 +0100

    Not neccessary anymore

--- app/presenters/DiscussionPresenter.php
+++ app/presenters/DiscussionPresenter.php
@@ -3,7 +3,6 @@
 namespace App\Presenters;
 
 use Nette\Application\UI\NewPostControl;
-use Tapi\DiscussionDetailResource;
 use Tapi\DiscussionListResource;
 use Tapi\DiscussionNewsListResource;
 use Tapi\DiscussionPageResource;
