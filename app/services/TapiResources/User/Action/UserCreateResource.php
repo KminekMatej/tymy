e6003e4d3f0f1047d3fd35db4280ef6ccbc9ca24
Matej Kminek <matej.kminek@attendees.eu>
2018-01-24 09:41:28 +0100

    User creator fixed

--- app/services/TapiResources/User/Action/UserCreateResource.php
+++ app/services/TapiResources/User/Action/UserCreateResource.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Tapi;
+use Tapi\Exception\APIException;
 
 /**
  * Project: tymy_v2
@@ -14,21 +15,31 @@ class UserCreateResource extends UserResource {
         parent::globalInit();
         $this->setCacheable(FALSE);
         $this->setMethod(RequestMethod::POST);
+        $this->setUser(NULL);
         return $this;
     }
 
     protected function preProcess() {
-        if ($this->getUser())
+        if (!$this->getUser())
             throw new APIException('User object not set');
         
         $this->setUrl("users");
-        $this->setRequestData($this->user);
+        $this->setRequestData($this->getUser());
 
         return $this;
     }
     
     protected function postProcess() {
         parent::postProcessUser($this->data);
+    }
+    
+    public function getUser() {
+        return $this->options->user;
+    }
+
+    public function setUser($user) {
+        $this->options->user = $user;
+        return $this;
     }
     
 }
