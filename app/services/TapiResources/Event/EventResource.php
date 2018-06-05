cf8842fc732425dc3507374c9ecf8423c945a21c
Matej Kminek <matej.kminek@attendees.eu>
2018-06-05 10:55:25 +0200

    MyAttendance property is added only if it doesnt exist in the first place (and if there is attendance property specified) Task #88 - V seznamu událostí v agendě nesvítí správně tlačítka se zadanou docházkou

--- app/services/TapiResources/Event/EventResource.php
+++ app/services/TapiResources/Event/EventResource.php
@@ -23,39 +23,12 @@ abstract class EventResource extends TapiObject {
         $event->resultsClosed = false;
         $event->isPast = $event->endTime < new \Nette\Utils\DateTime();
         
-        
-        $myAttendance = new \stdClass();
-        $myAttendance->preStatus = "UNKNOWN";
-        $myAttendance->postStatus = "UNKNOWN";
-        $myAttendance->preDescription = "";
-        $myAttendance->postDescription = "";
         if (!property_exists($event, "place"))
             $event->place = ""; //set default value
         if (!property_exists($event, "link"))
             $event->link = ""; //set default value
-        if (property_exists($event, "attendance"))
-            foreach ($event->attendance as $att) {
-                if (!property_exists($att, "preStatus"))
-                    $att->preStatus = "UNKNOWN"; //set default value
-                if (!property_exists($att, "preDescription"))
-                    $att->preDescription = ""; //set default value
-                if (!property_exists($att, "postStatus")) {
-                    $att->postStatus = "UNKNOWN"; //set default value
-                } else {
-                    $event->resultsClosed = true;
-                }
-                if (!property_exists($att, "postDescription"))
-                    $att->postDescription = ""; //set default value
-                if (property_exists($att, "preDatMod"))
-                    $this->timeLoad($att->preDatMod);
-                if (property_exists($att, "postDatMod"))
-                    $this->timeLoad($att->postDatMod);
-                if ($att->userId == $this->user->getId()) {
-                    $myAttendance = $att;
-                }
-            }
-        $event->myAttendance = $myAttendance;
         
+        $this->addMyAttendance($event);
         if(property_exists($event, "myAttendance") && property_exists($event->myAttendance, "preStatus")){
                 $eventClassMap = [
                     "YES" => "success",
@@ -81,10 +54,50 @@ abstract class EventResource extends TapiObject {
         return $eventColor;
     }
     
-    private function addMyAttendance($event){
+    private function addMyAttendance(&$event) {
+        if (property_exists($event, "myAttendance")){
+            if(!property_exists($event->myAttendance, "postStatus")){
+                $event->myAttendance->postStatus = "UNKNOWN";
+                $event->myAttendance->postDescription = "";
+            }
+            if(!property_exists($event->myAttendance, "preStatus")){
+                $event->myAttendance->preStatus = "UNKNOWN";
+                $event->myAttendance->preDescription = "";
+            }
+            return;
+        }
         
+        $myAttendance = new \stdClass();
+        $myAttendance->preStatus = "UNKNOWN";
+        $myAttendance->postStatus = "UNKNOWN";
+        $myAttendance->preDescription = "";
+        $myAttendance->postDescription = "";
+
+        if (property_exists($event, "attendance")) {
+            foreach ($event->attendance as $att) {
+                if (!property_exists($att, "preStatus"))
+                    $att->preStatus = "UNKNOWN"; //set default value
+                if (!property_exists($att, "preDescription"))
+                    $att->preDescription = ""; //set default value
+                if (!property_exists($att, "postStatus")) {
+                    $att->postStatus = "UNKNOWN"; //set default value
+                } else {
+                    $event->resultsClosed = true;
+                }
+                if (!property_exists($att, "postDescription"))
+                    $att->postDescription = ""; //set default value
+                if (property_exists($att, "preDatMod"))
+                    $this->timeLoad($att->preDatMod);
+                if (property_exists($att, "postDatMod"))
+                    $this->timeLoad($att->postDatMod);
+                if ($att->userId == $this->user->getId()) {
+                    $myAttendance = $att;
+                }
+            }
+        }
+        $event->myAttendance = $myAttendance;
     }
-    
+
     protected function clearCache($id = NULL){
         $this->cache->clean([Cache::TAGS => "GET:events"]);
         $this->cache->clean([Cache::TAGS => "GET:events/withMyAttendance"]);
