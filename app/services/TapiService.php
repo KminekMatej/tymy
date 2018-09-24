320f7d56921507414aa8dcaaaecafabcf462abbb
Matej Kminek <matej.kminek@attendees.eu>
2018-09-24 13:12:45 +0200

    CURL requests should follow redirects

--- app/services/TapiService.php
+++ app/services/TapiService.php
@@ -122,6 +122,7 @@ class TapiService {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         if($this->supplier->isHttps()){
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
