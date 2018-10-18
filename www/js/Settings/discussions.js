6160cf14638cc86b47871feea8b707060eb33db6
Matej Kminek <matej.kminek@attendees.eu>
2018-10-18 22:45:08 +0200

    Binder default values for checkboxes are 1 and 0 Task #104 - User edit - Pokazde validacni error

--- www/js/Settings/discussions.js
+++ www/js/Settings/discussions.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     $("[data-binder-id]").each(function () {
         $(this).data("data-binder", new Binder({
             area: this,
+            checkboxValueChecked: 1,
+            checkboxValueUnChecked: 0,
             isValid: function (name, value1, value2) {
                 switch (name) {
                     case "caption":
