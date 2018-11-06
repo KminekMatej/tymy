e8991d5aa831e264febc0f2a0a80d3c843bde82c
Matej Kminek <matej.kminek@attendees.eu>
2018-11-06 21:21:19 +0100

    Typo

--- www/js/Discussion/Discussion.js
+++ www/js/Discussion/Discussion.js
@@ -40,7 +40,7 @@ function updatePost(url) {
 }
 
 function deletePost(id, url) {
-    if (!confirm(translate.dicussion.alerts.confirmDeletePost)) {
+    if (!confirm(translate.discussion.alerts.confirmDeletePost)) {
         return;
     }
     postElm = $("DIV#row-" + id)
