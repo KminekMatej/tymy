5d3f09ed0d21618e57f7e0a0967b5c4f7416c327
Matej Kminek <matej.kminek@attendees.eu>
2019-01-16 08:19:20 +0100

    Shell scripts for easy testing

--- tests/test_DEV_COMPONENTS.sh
+++ tests/test_DEV_COMPONENTS.sh
@@ -4,6 +4,6 @@ RUN_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 chmod -R 0777 $RUN_PATH/*
 chmod -R 0777 $RUN_PATH/../temp/*
 chmod -R 0777 $RUN_PATH/../log/*
-TESTCMD="$RUN_PATH/../vendor/nette/tester/src/tester.php -p php -c $RUN_PATH/php.ini -j 4 --setup $RUN_PATH/setup_MONKEYS.php $RUN_PATH"
+TESTCMD="$RUN_PATH/../vendor/nette/tester/src/tester.php -p php -c $RUN_PATH/php.ini -j 4 --setup $RUN_PATH/setup_DEV.php $RUN_PATH/components"
 echo $TESTCMD
 php $TESTCMD
--- tests/test_DEV_PRESENTERS.sh
+++ tests/test_DEV_PRESENTERS.sh
@@ -2,6 +2,8 @@
 
 RUN_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 chmod -R 0777 $RUN_PATH/*
-chmod -R 0777 $RUN_PATH/../tmp/*
+chmod -R 0777 $RUN_PATH/../temp/*
 chmod -R 0777 $RUN_PATH/../log/*
-php $RUN_PATH/../vendor/nette/tester/src/tester.php -p php -c $RUN_PATH/php.ini --setup $RUN_PATH/setup_XDEV.php $RUN_PATH/
+TESTCMD="$RUN_PATH/../vendor/nette/tester/src/tester.php -p php -c $RUN_PATH/php.ini -j 4 --setup $RUN_PATH/setup_DEV.php $RUN_PATH/presenters/"
+echo $TESTCMD
+php $TESTCMD
