642648ae9c0df06b8072cf771174b58704be1b4b
Matej Kminek <matej.kminek@attendees.eu>
2021-05-15 21:42:12 +0200

    feat: Edit post refactor, fix uses and auto format Signed-off-by:Matej Kminek <matej.kminek@attendees.eu>

--- app/presenters/DiscussionPresenter.php
+++ app/presenters/DiscussionPresenter.php
@@ -3,7 +3,6 @@
 namespace Tymy\App\Presenters;
 
 use Nette\Application\UI\NewPostControl;
-use Tapi\Exception\APIException;
 use Tymy\Module\Discussion\Manager\DiscussionManager;
 use Tymy\Module\Discussion\Manager\PostManager;
 use Tymy\Module\User\Manager\UserManager;
@@ -13,27 +12,24 @@ use Tymy\Module\User\Manager\UserManager;
  *
  * @author matej
  */
-class DiscussionPresenter extends SecuredPresenter {
-    public $discussionPage;
-    public $discussionsList;
-    public $discussionPostCreate;
-    public $discussionPostEdit;
-    public $discussionPostDelete;
-    
+class DiscussionPresenter extends SecuredPresenter
+{
     /** @inject */
     public DiscussionManager $discussionManager;
-    
+
     /** @inject */
     public PostManager $postManager;
-    
+
     /** @inject */
     public UserManager $userManager;
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
     }
-    
-    public function startup() {
+
+    public function startup()
+    {
         parent::startup();
         $this->setLevelCaptions(["1" => ["caption" => $this->translator->translate("discussion.discussion", 2), "link" => $this->link("Discussion:")]]);
     }
@@ -43,10 +39,11 @@ class DiscussionPresenter extends SecuredPresenter {
         $this->template->discussions = $this->discussionManager->getListUserAllowed($this->user->getId());
     }
 
-    public function actionNewPost($discussion){
+    public function actionNewPost($discussion)
+    {
         $post = $this->getHttpRequest()->getPost("post");
         if (trim($post) != "") {
-            $this->postManager->createByArray([
+            $this->postManager->create([
                 "post" => $post,
                 "discussionId" => $discussion,
                 "createdById" => $this->user->getId(),
@@ -55,20 +52,17 @@ class DiscussionPresenter extends SecuredPresenter {
         $this->setView('discussion');
     }
 
-    public function actionEditPost($discussion) {
+    public function actionEditPost($discussion)
+    {
         $postId = $this->getHttpRequest()->getPost("postId");
         $text = $this->getHttpRequest()->getPost("post");
-        $sticky = $this->getHttpRequest()->getPost("sticky") ;
-        try {
-            $this->discussionPostEdit->init()
-                        ->setId($discussion)
-                        ->setPostId($postId)
-                        ->setPost($text)
-                        ->setSticky($sticky ? $sticky == "true" : null) //super cool determining if sticky is true, false or null
-                        ->perform();
-        } catch (APIException $ex) {
-            $this->handleTapiException($ex, 'this');
-        }
+        $sticky = $this->getHttpRequest()->getPost("sticky");
+
+        $this->postManager->update([
+            "post" => $text,
+            "sticky" => $sticky,
+                ], $discussion, $postId);
+
         $this->setView('discussion');
     }
 
@@ -82,33 +76,34 @@ class DiscussionPresenter extends SecuredPresenter {
     public function actionStickPost($postId, $discussionId, $sticky)
     {
         $this->postManager->stickPost($postId, $discussionId, $sticky ? true : false);
-        
+
         $this->redirect("Discussion:discussion", ["discussion" => $discussionId, "page" => 1]);
     }
 
-    public function renderDiscussion($discussion, $page, $search, $suser = "all", $jump2date = "") {
-        
+    public function renderDiscussion($discussion, $page, $search, $suser = "all", $jump2date = "")
+    {
+
         $d = (is_int($discussion) || is_numeric($discussion)) ? $this->discussionManager->getById(intval($discussion)) : $this->discussionManager->getByWebName($this->user->getId(), $discussion);
 
-        if (empty($d)){
+        if (empty($d)) {
             $this->error($this->translator->translate("discussion.errors.noDiscussionExists"));
-        }  
-        
+        }
+
         $this->template->search = $search;
         $this->template->suser = $suser;
         $this->template->jump2date = $jump2date;
-        
+
         $discussionPosts = $this->postManager->mode($d->getId(), $page, "html", $search, $suser, $jump2date);
-        
+
         //set users
         $this->template->userList = $this->userManager->getList();
-        
-        $this->setLevelCaptions(["2" => ["caption" => $d->getCaption(), "link" => $this->link("Discussion:discussion", [$d->getWebName()]) ] ]);
-        
+
+        $this->setLevelCaptions(["2" => ["caption" => $d->getCaption(), "link" => $this->link("Discussion:discussion", [$d->getWebName()])]]);
+
         $this->template->userId = $this->getUser()->getId();
         $this->template->discussionPosts = $discussionPosts;
         $this->template->nazevDiskuze = $discussionPosts->getDiscussion()->getWebName();
-        $currentPage = is_numeric($discussionPosts->getCurrentPage()) ? $discussionPosts->getCurrentPage() : 1 ;
+        $currentPage = is_numeric($discussionPosts->getCurrentPage()) ? $discussionPosts->getCurrentPage() : 1;
         $this->template->currentPage = $currentPage;
         $lastPage = is_numeric($discussionPosts->getNumberOfPages()) ? $discussionPosts->getNumberOfPages() : 1;
         $this->template->lastPage = $lastPage;
@@ -118,10 +113,10 @@ class DiscussionPresenter extends SecuredPresenter {
         }
     }
 
-    protected function createComponentNewPost() {
+    protected function createComponentNewPost()
+    {
         $newpost = new NewPostControl($this->userManager);
         $newpost->redrawControl();
         return $newpost;
     }
-     
 }
