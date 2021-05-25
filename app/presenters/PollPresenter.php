0e79de18ba6a3b78c2b9698cbfe39cd21378c535
Matej Kminek <m@kminet.eu>
2021-05-25 23:09:56 +0200

    feat: Polls refactoring

--- app/presenters/PollPresenter.php
+++ app/presenters/PollPresenter.php
@@ -2,75 +2,64 @@
 
 namespace Tymy\App\Presenters;
 
-use Nette;
-use Nette\Application\UI\NavbarControl;
-use Tymy\App\Model;
-use Nette\Application\UI\Form;
-use Nette\Utils\Strings;
-use Tapi\PollDetailResource;
-use Tapi\PollListResource;
-use Tapi\PollVoteResource;
-use Tapi\Exception\APIException;
+use Tymy\Module\Poll\Manager\PollManager;
+use Tymy\Module\Poll\Manager\VoteManager;
+use Tymy\Module\Poll\Model\Poll;
 
-class PollPresenter extends SecuredPresenter {
+class PollPresenter extends SecuredPresenter
+{
+    /** @inject */
+    public PollManager $pollManager;
+    /** @inject */
+    public VoteManager $voteManager;
 
-    public $navbar;
-    public $pollList;
-    public $poll;
-    public $pollVoter;
-    
-    public function startup() {
+    public function startup()
+    {
         parent::startup();
         $this->setLevelCaptions(["1" => ["caption" => $this->translator->translate("poll.poll", 2), "link" => $this->link("Poll:")]]);
     }
-    
-    public function renderDefault() {
-        //parent::showNotes();
-        try {
-            $this->template->polls = $this->pollList->init()->setMenu(TRUE)->getData();
-        } catch (APIException $ex) {
-            $this->handleTapiException($ex);
-        }
+
+    public function renderDefault()
+    {
+        $this->template->polls = $this->pollManager->getListMenu();
     }
-    
-    public function renderPoll($anketa) {
-                try {
-        $poll = $this->poll->init()
-                ->setId($this->parseIdFromWebname($anketa))
-                ->getData();
-        $this->userList->init()->getData();
-        $this->template->users = $this->userList->getById();
-        } catch (APIException $ex) {
-            $this->handleTapiException($ex);
-        }
-        //parent::showNotes($poll->id);
-        $this->setLevelCaptions(["2" => ["caption" => $poll->caption, "link" => $this->link("Poll:poll", $poll->webName) ] ]);
-        
+
+    public function renderPoll($anketa)
+    {
+        /* @var $poll Poll */
+        $poll = $this->pollManager->getById($this->parseIdFromWebname($anketa));
+        $this->template->users = $this->userManager->getIdList();
+
+        $this->setLevelCaptions(["2" => ["caption" => $poll->getCaption(), "link" => $this->link("Poll:poll", $poll->getWebName())]]);
+
         $this->template->poll = $poll;
-        
+
         $this->template->resultsDisplayed = $this->translator->translate("poll.resultsDisplayed");
         $this->template->resultsDisplayedAfterVote = $this->translator->translate("poll.resultsDisplayedAfterVote");
         $this->template->resultsToYouOnly = $this->translator->translate("poll.resultsToYouOnly");
         $this->template->resultsAreSecret = $this->translator->translate("poll.resultsAreSecret");
         $this->template->resultsDisplayedWhenClosed = $this->translator->translate("poll.resultsDisplayedWhenClosed");
-
     }
-    
-    public function handleVote($pollId) {
+
+    public function handleVote($pollId)
+    {
         $votes = [];
         $post = $this->getRequest()->getPost();
         foreach ($post as $optId => $opt) {
-            if (array_key_exists("value", $opt)) {
-                $votes[] = ["optionId" => $optId, $opt["type"] => $opt["type"] == "numericValue" ? (int) $opt["value"] : $opt["value"]];
+            if (!array_key_exists("value", $opt)) {
+                continue;
             }
+
+            $votes[] = [
+                "optionId" => $optId,
+                "userId" => $this->user->getId(),
+                $opt["type"] => $opt["type"] == "numericValue" ? (int) $opt["value"] : $opt["value"]
+            ];
         }
         $this->redrawControl("poll-results");
         $this->redrawNavbar();
-        try {
-            $this->pollVoter->init()->setId($pollId)->setVotes($votes)->perform();
-        } catch (APIException $ex) {
-            $this->handleTapiException($ex, "this");
-        }
+
+        $this->voteManager->create($votes, $pollId);
     }
 
 }
