f78e6418002575a44f7e9b6fc9a38b77a54809e8
Matej Kminek <m@kminet.eu>
2021-09-11 21:22:05 +0200

    feat: Remove feed remains

--- app/presenters/EventPresenter.php
+++ app/presenters/EventPresenter.php
@@ -169,40 +169,6 @@ class EventPresenter extends SecuredPresenter
         $this->template->myPostStatusCaption = $eventCaptions["myPostStatusCaption"];
     }
 
-    public function actionFeed(string $start, string $end)
-    {
-        $events = $this->eventManager->getEventsInterval($this->user->getId(), new DateTime($start), new DateTime($end));
-
-        $this->sendResponse(new JsonResponse($this->toFeed($events)));
-    }
-
-    /**
-     * Transform array of events into event feed - array in format specified by FullCalendar specifications
-     * 
-     * @param Event[] $events
-     * @return array
-     */
-    private function toFeed(array $events)
-    {
-        $feed = [];
-
-        foreach ($events as $event) {
-            /* @var $event Event */
-            $feed[] = [
-                "id" => $event->getId(),
-                "title" => $event->getCaption(),
-                "start" => $event->getStartTime()->format(BaseModel::DATETIME_ISO_FORMAT),
-                "end" => $event->getEndTime()->format(BaseModel::DATETIME_ISO_FORMAT),
-                "backgroundColor" => $event->getBackgroundColor(),
-                "borderColor" => $event->getBorderColor(),
-                "textColor" => $event->getTextColor(),
-                "url" => $this->link("Event:event", $event->getWebName()),
-            ];
-        }
-
-        return $feed;
-    }
-
     public function handleAttendance($id, $code, $desc)
     {
         $this->attendanceManager->create([
