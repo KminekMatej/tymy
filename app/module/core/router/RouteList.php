8cb3b2172755ad70de09af2998db4a7c8cdf9a35
Matej Kminek <m@kminet.eu>
2021-09-22 21:44:18 +0200

    feat: Rewrite api routes to use native functions

--- app/module/core/router/RouteList.php
+++ app/module/core/router/RouteList.php
@@ -2,9 +2,7 @@
 
 namespace Tymy\Module\Core\Router;
 
-use Nette\Application\Routers\Route;
 use Nette\Application\Routers\RouteList as NetteRouteList;
-
 
 /**
  * Description of RouteList
@@ -16,17 +14,8 @@ class RouteList extends NetteRouteList
 
     public function addApiRoute(string $mask, $metadata = [], int $flags = 0)
     {
-        $metadata['module'] = [
-            Route::VALUE => isset($metadata['module']) ? "Api:" . $metadata['module'] : null,
-            Route::FILTER_IN => function ($module) {
-                return 'Api:' . ucfirst($module);
-            },
-            Route::FILTER_OUT => function ($module) {
-                return is_array($module) ? strtolower(explode(':', $module)[1]) : $module;
-            },
-        ];
-
-        return parent::addRoute("/api/" . $mask, $metadata, $flags);
+        $this->withPath("api")->withModule("Api")->addRoute($mask, $metadata, $flags);
+        return $this;
     }
 
 }
