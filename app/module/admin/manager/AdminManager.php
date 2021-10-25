28238425cd1c96ebd3f880b480e79e1df389523a
Matej Kminek <m@kminet.eu>
2021-10-25 11:17:37 +0200

    feat: Add AdminManager to allow access only superadmins

---
+++ app/module/admin/manager/AdminManager.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tymy\Module\Admin\Manager;
+
+/**
+ * Description of AdminManager
+ *
+ * @author kminekmatej, 25. 10. 2021
+ */
+class AdminManager
+{
+
+    private array $ghosts;
+
+    public function __construct(array $ghosts)
+    {
+        $this->ghosts = $ghosts;
+    }
+
+    /**
+     * Returns validity of given token
+     * 
+     * @param string $token
+     * @return bool
+     */
+    public function allowToken(string $token): bool
+    {
+        return in_array($token, $this->ghosts);
+    }
+}
---
+++ app/module/admin/presenter/api/AdminSecuredPresenter.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Tymy\Module\Admin\Presenter\Api;
+
+use Tymy\Module\Admin\Manager\AdminManager;
+use Tymy\Module\Core\Presenter\Api\BasePresenter;
+
+/**
+ * Description of AdminSecuredPresenter
+ *
+ * @author kminekmatej, 25. 10. 2021
+ */
+class AdminSecuredPresenter extends BasePresenter
+{
+
+    /** @inject */
+    public AdminManager $adminManager;
+
+    protected function startup()
+    {
+        parent::startup();
+
+        $token = $this->getTokenFromHeader('Authorization') ?: $this->getTokenFromHeader('X-Authorization') ?: $this->getTokenFromUrl('token');
+
+        if (!$token || !$this->adminManager->allowToken($token)) {
+            $this->responder->E401_UNAUTHORIZED();
+        }
+    }
+
+    /**
+     * Attempts to load Admin token from specified header
+     * If there is something sent inside specified header, always logout current user and work just with this header
+     *
+     * @param string $header Name of header to try to parse from
+     * @return string|null If bearer is not found or is invalid formatted
+     */
+    private function getTokenFromHeader($header): ?string
+    {
+        $headerContent = $this->getHttpRequest()->getHeader($header);
+
+        if (empty($headerContent)) {
+            return null;
+        }
+
+        if ($this->user->isLoggedIn()) {
+            $this->user->logout(true); //
+        }
+
+        return is_string($headerContent) ? $headerContent : null;
+    }
+
+    /**
+     * Attempts to load Bearer token from specified url param
+     * If there is something sent in this param, always logout current user and work just with this header
+     *
+     * @param string $param Name of url param to try to parse from
+     * @return string|null If bearer is not found or is invalid formatted
+     */
+    private function getTokenFromUrl(string $param): ?string
+    {
+        $paramContent = $this->getHttpRequest()->getUrl()->getQueryParameter($param);
+
+        if (empty($paramContent)) {
+            return null;
+        }
+
+        if ($this->user->isLoggedIn()) {
+            $this->user->logout(true); //
+        }
+
+        return is_string($paramContent) ? $paramContent : null;
+    }
+
+    /**
+     * Authorize user using token
+     *
+     * @param string $token Contents
+     */
+    private function allowToken(?string $token): void
+    {
+    }
+
+}
