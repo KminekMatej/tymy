b538c9e7a0ab7f71e80481570504df21004e707f
Matěj Kmínek <m@kminet.eu>
2021-10-27 22:30:59 +0200

    feat: Refactorinfg of Debt module

--- app/module/debt/presenter/front/DefaultPresenter.php
+++ app/module/debt/presenter/front/DefaultPresenter.php
@@ -6,10 +6,7 @@ use Nette\Utils\DateTime;
 use Nette\Utils\Strings;
 use QrCode\QRcode;
 use Tapi\TapiObject;
-use Tymy\Module\Core\Presenter\Front\SecuredPresenter;
-use Tymy\Module\Debt\Manager\DebtManager;
 use Tymy\Module\Debt\Model\Debt;
-use Tymy\Module\Permission\Model\Privilege;
 use const QR_ECLEVEL_H;
 use function iban_set_checksum;
 
@@ -18,17 +15,8 @@ use function iban_set_checksum;
  *
  * @author Matej Kminek <matej.kminek@attendees.eu>, 10. 2. 2020
  */
-class DefaultPresenter extends SecuredPresenter
+class DefaultPresenter extends DebtBasePresenter
 {
-
-    /** @inject */
-    public DebtManager $debtManager;
-
-    public function beforeRender()
-    {
-        parent::beforeRender();
-        $this->setLevelCaptions(["1" => ["caption" => $this->translator->translate("debt.debt", 2), "link" => $this->link(":Debt:Default:")]]);
-    }
 
     public function actionDefault(?string $resource = null)
     {
@@ -60,9 +48,9 @@ class DefaultPresenter extends SecuredPresenter
         $this->template->countryList = $this->getCountryList();
     }
 
-    public function renderDebtImg($dluh)
+    public function renderImg(string $resource)
     {
-        $debtId = $this->parseIdFromWebname($dluh);
+        $debtId = $this->parseIdFromWebname($resource);
 
         /* @var $debt Debt */
         $debt = $this->debtManager->getById($debtId);
@@ -100,20 +88,6 @@ class DefaultPresenter extends SecuredPresenter
         $this->template->userListWithTeam = $this->userManager->getByIdWithTeam();
         $this->template->payeeList = $this->getPayeeList();
         $this->template->countryList = $this->getCountryList();
-    }
-
-    /**
-     * Get list of possible payees - always Me, but if user is permitted to manage team debts, also TEAM mocked user
-     * 
-     * @return array
-     */
-    private function getPayeeList(): array
-    {
-        $payeeList = [$this->userManager->getById($this->user->getId())];
-        if ($this->getUser()->isAllowed($this->user->getId(), Privilege::SYS("DEBTS_TEAM"))) {
-            $payeeList[] = $this->userManager->mockTeamUser();
-        }
-        return $payeeList;
     }
 
     private function generateQRCodeString($payeeCallName, $payeeEmail, $accountNumber, $amount, $varcode, $message, $currencyISO = "CZK", $countryISO = "CZ")
@@ -186,275 +160,16 @@ class DefaultPresenter extends SecuredPresenter
         $this->editDebt($bind);
     }
 
-    public function handleDebtDelete()
+    public function handleDebtDelete(string $resource)
     {
-        $this->debtManager->delete($bind["id"]);
+        $debtId = $this->parseIdFromWebname($resource);
+        $this->debtManager->delete($debtId);
+        $this->redirect(":Core:Default:");
     }
 
     private function editDebt($bind)
     {
         $this->debtManager->update($bind["changes"], $bind["id"]);
-    }
-
-    private function getCountryList()
-    {
-        return [
-            'AF' => 'Afghánistán',
-            'AX' => 'Ålandy',
-            'AL' => 'Albánie',
-            'DZ' => 'Alžírsko',
-            'AS' => 'Americká Samoa',
-            'VI' => 'Americké Panenské ostrovy',
-            'AD' => 'Andorra',
-            'AO' => 'Angola',
-            'AI' => 'Anguilla',
-            'AQ' => 'Antarktida',
-            'AG' => 'Antigua a Barbuda',
-            'AR' => 'Argentina',
-            'AM' => 'Arménie',
-            'AW' => 'Aruba',
-            'AC' => 'Ascension',
-            'AU' => 'Austrálie',
-            'AZ' => 'Ázerbájdžán',
-            'BS' => 'Bahamy',
-            'BH' => 'Bahrajn',
-            'BD' => 'Bangladéš',
-            'BB' => 'Barbados',
-            'BE' => 'Belgie',
-            'BZ' => 'Belize',
-            'BY' => 'Bělorusko',
-            'BJ' => 'Benin',
-            'BM' => 'Bermudy',
-            'BT' => 'Bhútán',
-            'BO' => 'Bolívie',
-            'BA' => 'Bosna a Hercegovina',
-            'BW' => 'Botswana',
-            'BR' => 'Brazílie',
-            'IO' => 'Britské indickooceánské území',
-            'VG' => 'Britské Panenské ostrovy',
-            'BN' => 'Brunej',
-            'BG' => 'Bulharsko',
-            'BF' => 'Burkina Faso',
-            'BI' => 'Burundi',
-            'EA' => 'Ceuta a Melilla',
-            'CK' => 'Cookovy ostrovy',
-            'CW' => 'Curaçao',
-            'TD' => 'Čad',
-            'ME' => 'Černá Hora',
-            'CZ' => 'Česko',
-            'CN' => 'Čína',
-            'DK' => 'Dánsko',
-            'DG' => 'Diego García',
-            'DM' => 'Dominika',
-            'DO' => 'Dominikánská republika',
-            'DJ' => 'Džibutsko',
-            'EG' => 'Egypt',
-            'EC' => 'Ekvádor',
-            'ER' => 'Eritrea',
-            'EE' => 'Estonsko',
-            'ET' => 'Etiopie',
-            'FO' => 'Faerské ostrovy',
-            'FK' => 'Falklandské ostrovy',
-            'FJ' => 'Fidži',
-            'PH' => 'Filipíny',
-            'FI' => 'Finsko',
-            'FR' => 'Francie',
-            'GF' => 'Francouzská Guyana',
-            'TF' => 'Francouzská jižní území',
-            'PF' => 'Francouzská Polynésie',
-            'GA' => 'Gabon',
-            'GM' => 'Gambie',
-            'GH' => 'Ghana',
-            'GI' => 'Gibraltar',
-            'GD' => 'Grenada',
-            'GL' => 'Grónsko',
-            'GE' => 'Gruzie',
-            'GP' => 'Guadeloupe',
-            'GU' => 'Guam',
-            'GT' => 'Guatemala',
-            'GG' => 'Guernsey',
-            'GN' => 'Guinea',
-            'GW' => 'Guinea-Bissau',
-            'GY' => 'Guyana',
-            'HT' => 'Haiti',
-            'HN' => 'Honduras',
-            'HK' => 'Hongkong – ZAO Číny',
-            'CL' => 'Chile',
-            'HR' => 'Chorvatsko',
-            'IN' => 'Indie',
-            'ID' => 'Indonésie',
-            'IQ' => 'Irák',
-            'IR' => 'Írán',
-            'IE' => 'Irsko',
-            'IS' => 'Island',
-            'IT' => 'Itálie',
-            'IL' => 'Izrael',
-            'JM' => 'Jamajka',
-            'JP' => 'Japonsko',
-            'YE' => 'Jemen',
-            'JE' => 'Jersey',
-            'ZA' => 'Jihoafrická republika',
-            'GS' => 'Jižní Georgie a Jižní Sandwichovy ostrovy',
-            'KR' => 'Jižní Korea',
-            'SS' => 'Jižní Súdán',
-            'JO' => 'Jordánsko',
-            'KY' => 'Kajmanské ostrovy',
-            'KH' => 'Kambodža',
-            'CM' => 'Kamerun',
-            'CA' => 'Kanada',
-            'IC' => 'Kanárské ostrovy',
-            'CV' => 'Kapverdy',
-            'BQ' => 'Karibské Nizozemsko',
-            'QA' => 'Katar',
-            'KZ' => 'Kazachstán',
-            'KE' => 'Keňa',
-            'KI' => 'Kiribati',
-            'CC' => 'Kokosové ostrovy',
-            'CO' => 'Kolumbie',
-            'KM' => 'Komory',
-            'CG' => 'Kongo – Brazzaville',
-            'CD' => 'Kongo – Kinshasa',
-            'XK' => 'Kosovo',
-            'CR' => 'Kostarika',
-            'CU' => 'Kuba',
-            'KW' => 'Kuvajt',
-            'CY' => 'Kypr',
-            'KG' => 'Kyrgyzstán',
-            'LA' => 'Laos',
-            'LS' => 'Lesotho',
-            'LB' => 'Libanon',
-            'LR' => 'Libérie',
-            'LY' => 'Libye',
-            'LI' => 'Lichtenštejnsko',
-            'LT' => 'Litva',
-            'LV' => 'Lotyšsko',
-            'LU' => 'Lucembursko',
-            'MO' => 'Macao – ZAO Číny',
-            'MG' => 'Madagaskar',
-            'HU' => 'Maďarsko',
-            'MY' => 'Malajsie',
-            'MW' => 'Malawi',
-            'MV' => 'Maledivy',
-            'ML' => 'Mali',
-            'MT' => 'Malta',
-            'MA' => 'Maroko',
-            'MH' => 'Marshallovy ostrovy',
-            'MQ' => 'Martinik',
-            'MU' => 'Mauricius',
-            'MR' => 'Mauritánie',
-            'YT' => 'Mayotte',
-            'UM' => 'Menší odlehlé ostrovy USA',
-            'MX' => 'Mexiko',
-            'FM' => 'Mikronésie',
-            'MD' => 'Moldavsko',
-            'MC' => 'Monako',
-            'MN' => 'Mongolsko',
-            'MS' => 'Montserrat',
-            'MZ' => 'Mosambik',
-            'MM' => 'Myanmar (Barma)',
-            'NA' => 'Namibie',
-            'NR' => 'Nauru',
-            'DE' => 'Německo',
-            'NP' => 'Nepál',
-            'NE' => 'Niger',
-            'NG' => 'Nigérie',
-            'NI' => 'Nikaragua',
-            'NU' => 'Niue',
-            'NL' => 'Nizozemsko',
-            'NF' => 'Norfolk',
-            'NO' => 'Norsko',
-            'NC' => 'Nová Kaledonie',
-            'NZ' => 'Nový Zéland',
-            'OM' => 'Omán',
-            'IM' => 'Ostrov Man',
-            'PK' => 'Pákistán',
-            'PW' => 'Palau',
-            'PS' => 'Palestinská území',
-            'PA' => 'Panama',
-            'PG' => 'Papua-Nová Guinea',
-            'PY' => 'Paraguay',
-            'PE' => 'Peru',
-            'PN' => 'Pitcairnovy ostrovy',
-            'CI' => 'Pobřeží slonoviny',
-            'PL' => 'Polsko',
-            'PR' => 'Portoriko',
-            'PT' => 'Portugalsko',
-            'AT' => 'Rakousko',
-            'RE' => 'Réunion',
-            'GQ' => 'Rovníková Guinea',
-            'RO' => 'Rumunsko',
-            'RU' => 'Rusko',
-            'RW' => 'Rwanda',
-            'GR' => 'Řecko',
-            'PM' => 'Saint-Pierre a Miquelon',
-            'SV' => 'Salvador',
-            'WS' => 'Samoa',
-            'SM' => 'San Marino',
-            'SA' => 'Saúdská Arábie',
-            'SN' => 'Senegal',
-            'KP' => 'Severní Korea',
-            'MK' => 'Severní Makedonie',
-            'MP' => 'Severní Mariany',
-            'SC' => 'Seychely',
-            'SL' => 'Sierra Leone',
-            'XA' => 'simulovaná diakritika',
-            'XB' => 'simulovaný obousměrný zápis',
-            'SG' => 'Singapur',
-            'SK' => 'Slovensko',
-            'SI' => 'Slovinsko',
-            'SO' => 'Somálsko',
-            'AE' => 'Spojené arabské emiráty',
-            'GB' => 'Spojené království',
-            'US' => 'Spojené státy',
-            'RS' => 'Srbsko',
-            'LK' => 'Srí Lanka',
-            'CF' => 'Středoafrická republika',
-            'SD' => 'Súdán',
-            'SR' => 'Surinam',
-            'SH' => 'Svatá Helena',
-            'LC' => 'Svatá Lucie',
-            'BL' => 'Svatý Bartoloměj',
-            'KN' => 'Svatý Kryštof a Nevis',
-            'MF' => 'Svatý Martin (Francie)',
-            'SX' => 'Svatý Martin (Nizozemsko)',
-            'ST' => 'Svatý Tomáš a Princův ostrov',
-            'VC' => 'Svatý Vincenc a Grenadiny',
-            'SZ' => 'Svazijsko',
-            'SY' => 'Sýrie',
-            'SB' => 'Šalamounovy ostrovy',
-            'ES' => 'Španělsko',
-            'SJ' => 'Špicberky a Jan Mayen',
-            'SE' => 'Švédsko',
-            'CH' => 'Švýcarsko',
-            'TJ' => 'Tádžikistán',
-            'TZ' => 'Tanzanie',
-            'TH' => 'Thajsko',
-            'TW' => 'Tchaj-wan',
-            'TG' => 'Togo',
-            'TK' => 'Tokelau',
-            'TO' => 'Tonga',
-            'TT' => 'Trinidad a Tobago',
-            'TA' => 'Tristan da Cunha',
-            'TN' => 'Tunisko',
-            'TR' => 'Turecko',
-            'TM' => 'Turkmenistán',
-            'TC' => 'Turks a Caicos',
-            'TV' => 'Tuvalu',
-            'UG' => 'Uganda',
-            'UA' => 'Ukrajina',
-            'UY' => 'Uruguay',
-            'UZ' => 'Uzbekistán',
-            'CX' => 'Vánoční ostrov',
-            'VU' => 'Vanuatu',
-            'VA' => 'Vatikán',
-            'VE' => 'Venezuela',
-            'VN' => 'Vietnam',
-            'TL' => 'Východní Timor',
-            'WF' => 'Wallis a Futuna',
-            'ZM' => 'Zambie',
-            'EH' => 'Západní Sahara',
-            'ZW' => 'Zimbabwe',
-        ];
     }
 
 }
---
+++ app/module/debt/presenter/front/NewPresenter.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tymy\Module\Debt\Presenter\Front;
+
+use Nette\Utils\DateTime;
+use Tymy\Module\Core\Model\BaseModel;
+use Tymy\Module\Debt\Manager\DebtManager;
+use Tymy\Module\Debt\Model\Debt;
+
+/**
+ * Description of NewPresenter
+ *
+ * @author Matej Kminek <matej.kminek@attendees.eu>, 10. 2. 2020
+ */
+class NewPresenter extends DebtBasePresenter
+{
+
+    /** @inject */
+    public DebtManager $debtManager;
+
+    public function renderDefault()
+    {
+        $this->template->debt = (new Debt())
+            ->setAmount(1)
+            ->setCurrencyIso("CZK")
+            ->setCountryIso("CZ")
+            ->setCaption("")
+            ->setCreated(new DateTime())
+            ->setDebtorId(null)
+            ->setDebtorType("user")
+            ->setPayeeId($this->user->getId())
+            ->setPayeeType("user")
+            ->setDebtDate(new DateTime())
+            ->setPayeeAccountNumber("")
+            ->setVarcode(null)
+            ->setCanRead(true)
+            ->setCanEdit(true)
+            ->setCanSetSentDate(false)
+            ->setPaymentSent(null)
+            ->setPaymentReceived(null);
+
+        $this->template->userListWithTeam = $this->userManager->getByIdWithTeam();
+        $this->template->payeeList = $this->getPayeeList();
+        $this->template->countryList = $this->getCountryList();
+    }
+
+    public function handleDebtCreate()
+    {
+        $bind = $this->getRequest()->getPost();
+
+        /* @var $createdDebt Debt */
+        $createdDebt = $this->debtManager->create($bind["changes"]);
+
+        $this->flashMessage($this->translator->translate("common.alerts.debtAdded"), "success");
+
+        $this->redirect(":Debt:Default:", $createdDebt->getWebName());
+    }
+
+}
