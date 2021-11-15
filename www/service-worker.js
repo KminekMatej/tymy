ef4ce02d806e9a08ae7697f979decd7e273c452c
Matěj Kmínek <m@kminet.eu>
2021-11-15 12:28:01 +0100

    feat: Transform public key

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,22 +1,21 @@
 // add event listener to subscribe and send subscription to server
 self.addEventListener('activate', subscribe);
 // and listen to incomming push notifications
-//self.addEventListener('push', pnPopupNotification);
+self.addEventListener('push', processPush);
 // ... and listen to the click
 //self.addEventListener('notificationclick', pnNotificationClick);
 
 async function subscribe(event) {
-    console.log('Serviceworker: ddactivate event');
     try {
         var opt = {
-            applicationServerKey: 'BNFwVh2n9jBpGDMx3Kiz47ldm8GrSP74Ff4VL54v6AoPg01BnT2TtqdEXBm6M7C8Nx3PoqqPie7kESKgm8jkAog',
+            applicationServerKey: base64UrlToUint8Array('BNFwVh2n9jBpGDMx3Kiz47ldm8GrSP74Ff4VL54v6AoPg01BnT2TtqdEXBm6M7C8Nx3PoqqPie7kESKgm8jkAog'),
             userVisibleOnly: true
         };
 
         self.registration.pushManager.subscribe(opt)
                 .then((sub) => {
                     // subscription succeeded - send to server
-                    pnSaveSubscription(sub)
+                    saveSubscription(sub)
                             .then((response) => {
                                 console.log(response);
                             }).catch((e) => {
@@ -33,7 +32,7 @@ async function subscribe(event) {
     }
 }
 
-async function pnSaveSubscription(subscription) {
+async function saveSubscription(subscription) {
     // stringify object to post as body with HTTP-request
     var fetchdata = {
         method: 'post',
@@ -49,4 +48,20 @@ async function processPush(event) {
     console.log(event.data);
     // From here we can write the data to IndexedDB, send it to any open
     // windows, display a notification, etc.
+}
+
+function base64UrlToUint8Array(base64UrlData) {
+    const padding = '='.repeat((4 - base64UrlData.length % 4) % 4);
+    const base64 = (base64UrlData + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = atob(base64);
+    const buffer = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        buffer[i] = rawData.charCodeAt(i);
+    }
+
+    return buffer;
 }
