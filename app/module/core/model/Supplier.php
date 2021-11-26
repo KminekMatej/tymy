6eefb5493e76b60a9181e2c40eacc7a292f288f7
Matěj Kmínek <m@kminet.eu>
2021-11-26 22:15:46 +0100

    feat: Fix tag.log placement

--- app/module/core/model/Supplier.php
+++ app/module/core/model/Supplier.php
@@ -147,7 +147,7 @@ class Supplier {
     }
 
     public function setVersion() {
-        $taglog = file(__DIR__ . "/../tag.log");
+        $taglog = file(TEAM_DIR . "/app/tag.log");
         foreach ($taglog as $log) {
             $matches = [];
             preg_match("/([0-9]{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])\s([0-1][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])\s\+\d{4}\|(\d*)\.(\d*)\.(\d*)/", $log, $matches);
