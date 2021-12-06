807268c77aef8304f21585c913e2ca89d9f6c8ce
Matěj Kmínek <m@kminet.eu>
2021-12-06 11:29:39 +0100

    feat: Move www contents to public subdir, to enable symlinking

--- www/public/js/main.js
+++ www/public/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+s$(document).ready(function () {
     reTitle();
 
     const pushNotificationSuported = isPushNotificationSupported();
@@ -6,6 +6,15 @@ $(document).ready(function () {
         registerServiceWorker();
         requestPushPermission();
     }
+
+    $('#fileupload').fileupload({
+        dataType: 'json',
+        done: function (e, data) {
+            $.each(data.result.files, function (index, file) {
+                $('<p></p>').text(file.name).appendTo(document.body);
+            });
+        }
+    });
 
 });
 
@@ -73,25 +82,3 @@ function requestPushPermission() {
         }
     });
 }
-
-
-/**
- * shows a notification
- */
-/*function sendNotification() {
-    const img = "/images/jason-leung-HM6TMmevbZQ-unsplash.jpg";
-    const text = "Take a look at this brand new t-shirt!";
-    const title = "New Product Available";
-    const options = {
-        body: text,
-        icon: "/images/jason-leung-HM6TMmevbZQ-unsplash.jpg",
-        vibrate: [200, 100, 200],
-        tag: "new-product",
-        image: img,
-        badge: "https://spyna.it/icons/android-icon-192x192.png",
-        actions: [{action: "Detail", title: "View", icon: "https://via.placeholder.com/128/ff0000"}]
-    };
-    navigator.serviceWorker.ready.then(function (serviceWorker) {
-        serviceWorker.showNotification(title, options);
-    });
-}*/
