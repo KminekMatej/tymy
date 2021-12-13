c6721d0c4cbc93f8d00f928750974c4c1125a484
Matěj Kmínek <m@kminet.eu>
2021-12-13 15:29:04 +0100

    feat: Use hasKey and hasNotKey from parent

--- app/module/autotest/entity/Assert.php
+++ app/module/autotest/entity/Assert.php
@@ -11,40 +11,6 @@ use Tymy\Module\Autotest\SimpleResponse;
  */
 class Assert extends \Tester\Assert
 {
-    /**
-     * Asserts that a haystack (string or array) has an expected key.
-     */
-    public static function hasKey($key, $actual, string $description = null): void
-    {
-        self::$counter++;
-        if (is_array($actual)) {
-            if (!array_key_exists($key, $actual)) {
-                self::fail(self::describe('%1 should contain key %2', $description), $actual, $key);
-            }
-        } else {
-            self::fail(self::describe('%1 should be array', $description), $actual);
-        }
-    }
-
-    /**
-     * Asserts that a haystack (string or array) has not an expected key.
-     */
-    public static function hasNotKey($key, $actual, string $description = null): void
-    {
-        self::$counter++;
-        if (is_array($actual)) {
-            if (array_key_exists($key, $actual)) {
-                self::fail(self::describe('%1 should not contain key %2', $description), $actual, $key);
-            }
-        } else {
-            self::fail(self::describe('%1 should be array', $description), $actual);
-        }
-    }
-
-    private static function describe(string $reason, string $description = null): string
-    {
-        return ($description ? $description . ': ' : '') . $reason;
-    }
 
     /**
      * Asserts that tymy system response is in error format and that responded code equals to expected one
