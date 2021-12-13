928fa3411713a75daf26d6d9e54d2c5963074c37
Matěj Kmínek <m@kminet.eu>
2021-12-13 21:20:01 +0100

    feat: Cleaning

--- app/forms/PwdLostFormFactory.php
+++ app/forms/PwdLostFormFactory.php
@@ -1,48 +1,35 @@
 <?php
-
 namespace Tymy\App\Forms;
 
-use Nette;
 use Nette\Application\UI\Form;
-use Nette\Security\User;
-use Tymy\Module\Core\Model\Supplier;
+use Nette\SmartObject;
 
-class PwdLostFormFactory {
+class PwdLostFormFactory
+{
 
-    use Nette\SmartObject;
+    use SmartObject;
 
-    /** @var FormFactory */
-    private $factory;
+    private FormFactory $factory;
 
-    /** @var User */
-    private $user;
-    
-    /** @var Supplier */
-    private $supplier;
-    
-    private $tapi_config;
-        
-    public function __construct(FormFactory $factory, User $user, Supplier $supplier) {
+    public function __construct(FormFactory $factory)
+    {
         $this->factory = $factory;
-        $this->user = $user;
-        $this->supplier = $supplier;
-        $this->tapi_config = $supplier->getTapi_config();
     }
 
     /**
      * @return Form
      */
-    public function create() {
+    public function create(): Form
+    {
 
         $form = $this->factory->create();
 
         $form->addText('email')
-                ->setAttribute("placeholder", "E-mail")
-                ->addRule(Form::EMAIL, "Nesprávný formát e-mailu")
-                ->setRequired('Vyplňte e-mailovou adresu na kterou je Váš účet registrován');
-        
+            ->setAttribute("placeholder", "E-mail")
+            ->addRule(Form::EMAIL, "Nesprávný formát e-mailu")
+            ->setRequired('Vyplňte e-mailovou adresu na kterou je Váš účet registrován');
+
         $form->addSubmit('send', 'ZASLAT KÓD');
         return $form;
     }
-
 }
