b93a5244701bd145536a9b559bc9113c8f8954fb
Matěj Kmínek <m@kminet.eu>
2021-12-13 10:29:14 +0100

    feat: Add presenter for registering deviceId

---
+++ app/module/push-notification/presenter/api/ApplePresenter.php
@@ -0,0 +1,61 @@
+<?php
+namespace Tymy\Module\PushNotification\Presenter\Api;
+
+use Tymy\Module\Core\Presenter\Api\SecuredPresenter;
+use Tymy\Module\PushNotification\Manager\PushNotificationManager;
+use Tymy\Module\PushNotification\Model\Subscriber;
+
+/**
+ * Description of ApnsPresenter
+ */
+class ApplePresenter extends SecuredPresenter
+{
+
+    public function injectManager(PushNotificationManager $manager): void
+    {
+        $this->manager = $manager;
+    }
+
+    public function actionDefault($resourceId)
+    {
+        switch ($this->getRequest()->getMethod()) {
+            case 'POST':
+                $this->requestPost($resourceId);
+            // no break
+        }
+
+        $this->respondNotAllowed();
+    }
+
+    protected function requestPost($resourceId)
+    {
+        if (empty($this->requestData)) {
+            $this->respondBadRequest("Missing request data");
+        }
+
+        if (!is_string($this->requestData)) {
+            $this->respondBadRequest("Invalid device id");
+        }
+
+        $deviceId = $this->requestData;
+
+        /* @var $subscriber Subscriber */
+        $subscriber = $this->manager->getByUserAndSubscription($this->user->getId(), $deviceId);
+
+        if ($subscriber) {
+            $this->respondOk($subscriber->jsonSerialize());
+        }
+
+        $createdSubscription = $this->manager->create([
+            "userId" => $this->user->getId(),
+            "type" => Subscriber::TYPE_APNS,
+            "subscription" => $deviceId,
+        ]);
+
+        if (!$createdSubscription) {
+            $this->responder->E4011_CREATE_FAILED(Subscriber::MODULE);
+        }
+
+        $this->respondOkCreated($createdSubscription->jsonSerialize());
+    }
+}
