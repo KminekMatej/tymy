1b6c1636e7075df4ab500703a04c46ec3a61c21a
Matěj Kmínek <m@kminet.eu>
2022-01-03 16:09:33 +0100

    feat: Delete usr_mail record when user gets deleted

---
+++ app/module/admin/migrations/2022-01-03T16-09-11.sql
@@ -0,0 +1,26 @@
+
+-- SQL Migration file
+-- Author: Matěj Kmínek <m@kminet.eu>
+-- Created: 03.01.2022 16:09:11
+
+
+/*
+    Comment for this migration: (not neccessary, but can be handy)
+
+
+*/
+
+
+-- UP:
+-- commands that updates database shall be written here:
+
+ALTER TABLE `usr_mails` ADD FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- DOWN:
+-- commands that reverts updates from UP section shall be written here:
+
+CALL 'Migrations DOWN are for security reasons temporarily DISABLED. Impossible to migrate down from 2022-01-03T16-09-11, in file 2022-01-03T16-09-11.sql';
+
+-- DOWN migrations are disabled for security reasons.
+-- These migrations might often include removal of some existent database columns, which is very dangerous to do on production servers.
+-- Whenever we would migrate DOWN, then it would be impossible to migrate UP again, without loss of data.
