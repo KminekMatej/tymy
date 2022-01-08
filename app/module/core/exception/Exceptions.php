6a004da591d12639898b3ebda4dcab86c28fb4f0
Matěj Kmínek <m@kminet.eu>
2022-01-08 22:22:30 +0100

    feat: Extend TymyResponse by api response codes

--- app/module/core/exception/Exceptions.php
+++ app/module/core/exception/Exceptions.php
@@ -20,5 +20,49 @@ class MissingInputException extends Exception
 
 class TymyResponse extends Exception
 {
+
+    private bool $success = true;
+    private ?string $sessionKey = null;
+    private mixed $payload = null;
     
+    public function __construct(string $message = "", int $code = 0, mixed $payload, bool $success = true, ?string $sessionKey, \Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->success = $success;
+        $this->sessionKey = $sessionKey;
+        $this->payload = $payload;
+    }
+
+        public function getSuccess(): bool
+    {
+        return $this->success;
+    }
+
+    public function getSessionKey(): ?string
+    {
+        return $this->sessionKey;
+    }
+
+    public function getPayload(): mixed
+    {
+        return $this->payload;
+    }
+
+    public function setSuccess(bool $success)
+    {
+        $this->success = $success;
+        return $this;
+    }
+
+    public function setSessionKey(?string $sessionKey)
+    {
+        $this->sessionKey = $sessionKey;
+        return $this;
+    }
+
+    public function setPayload(mixed $payload)
+    {
+        $this->payload = $payload;
+        return $this;
+    }
 }
