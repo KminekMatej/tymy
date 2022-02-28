6a75b10244141259ee5ae6262f013abe51d6444c
Matěj Kmínek <matej.kminek@attendees.eu>
2022-02-28 14:40:45 +0100

    feat: Additional check of migration table

--- app/module/admin/manager/MigrationManager.php
+++ app/module/admin/manager/MigrationManager.php
@@ -370,7 +370,12 @@ class MigrationManager
 
         $migrationTableExists = in_array("migration", $tables);
         if ($migrationTableExists) {
-            return; //there is already migration table structure, no neeed to continue
+            if($this->teamDatabase->table("migration")->where("result", "OK")->count("id") == 0){   //migration table exists, but not containing any migration, so its some bloat, remove the table and continue
+                $this->teamDatabase->query("DROP TABLE IF EXISTS `migration`;")->fetch();
+                $migrationTableExists = false;
+            } else {
+                return; //there is already migration table structure, no neeed to continue
+            }
         }
 
         $usersTableExists = in_array("users", $tables);
