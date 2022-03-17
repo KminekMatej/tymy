0612b46242e99f72a587e42c2a0ef97cc2b85381
Matěj Kmínek <matej.kminek@attendees.eu>
2022-03-17 22:11:43 +0100

    feat: Check remote team credentials using local request

--- app/module/user/manager/UserManager.php
+++ app/module/user/manager/UserManager.php
@@ -316,18 +316,9 @@ class UserManager extends BaseManager
      */
     public function checkCredentials(Team $team, string $username, string $password): ?int
     {
-        $dbName = $team->getDbName();
-        $tables = $this->database->query("SHOW TABLES FROM $dbName")->fetchPairs();
-        $userTbl = in_array("user", $tables) ? "user" : "users";
-        $userRow = $this->database->query("SELECT * FROM $dbName.$userTbl WHERE user_name = ?", $username)->fetch();
+        $userId = file_get_contents("https://tymy.cz/api/check-credentials?username={$username}&teamId={$team->getId()}&password={$password}");    //this request is accessible from localhost only
 
-        if (!$userRow) {
-            return false;
-        }
-
-        $expectedPassword = $userRow->password;
-
-        return $this->authenticationManager->passwordMatch($password, $expectedPassword) ? $userRow->id : null;
+        return $userId ?? null;
     }
 
     /**
