58c0384825928a899a7f89ecc6387a60c2cb026b
Matěj Kmínek <matej.kminek@attendees.eu>
2022-05-02 23:04:40 +0200

    feat: Add file downloader

---
+++ app/module/file/presenter/front/DownloadPresenter.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tymy\Module\File\Presenter\Front;
+
+use Nette\Application\Responses\FileResponse;
+use Tymy\Module\Core\Presenter\Front\SecuredPresenter;
+use Tymy\Module\File\Handler\FileManager;
+
+/**
+ * Description of DownloadPresenter
+ *
+ * @author kminekmatej, 2. 5. 2022, 22:48:27
+ */
+class DownloadPresenter extends SecuredPresenter
+{
+
+    public function actionDefault(string $filename, string $folder = "/")
+    {
+        $folderNoSeparators = trim($folder, "/");
+        $fullpath = FileManager::DOWNLOAD_DIR . "/$folderNoSeparators/$filename";
+
+        if (!file_exists($fullpath)) {
+            $this->flashMessage("File $folderNoSeparators/$filename not found", 'warning');
+            $this->redirect(":File:Default:");
+        }
+
+        $this->sendResponse(new FileResponse($fullpath, null, null, true));
+    }
+}
---
+++ app/module/file/router/Router.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tymy\Module\File\Router;
+
+use Tymy\Module\Core\Router\RouteList;
+use Tymy\Module\Core\Interfaces\RouterInterface;
+
+/**
+ * Description of Router
+ *
+ * @author Matej Kminek <matej.kminek@attendees.eu>, 03. 11. 2020
+ */
+class Router implements RouterInterface
+{
+    public function addRoutes(RouteList &$router): void
+    {
+        $router->addRoute('file/download/<filename>', array(
+            'module' => 'File',
+            'presenter' => 'Download',
+            'action' => 'default',
+        ));
+    }
+}
