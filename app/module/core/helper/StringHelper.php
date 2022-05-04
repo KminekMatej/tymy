b4c6f7d588fccfe3dbd1cc26de3a075a2d745925
Matěj Kmínek <matej.kminek@attendees.eu>
2022-05-04 22:03:53 +0200

    feat: add StringHelper with extended urldecode and encode

---
+++ app/module/core/helper/StringHelper.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tymy\Module\Core\Helper;
+
+/**
+ * Description of StringHelper
+ *
+ * @author kminekmatej, 4. 5. 2022, 21:59:59
+ */
+class StringHelper
+{
+
+    /**
+     * Encode url, along with dots and hyphens
+     * @param string $input
+     * @return string
+     */
+    public static function urlencode(string $input): string
+    {
+        $str = urlencode($input);
+        $str = str_replace('.', '%2E', $str);
+        $str = str_replace('-', '%2D', $str);
+        return $str;
+    }
+
+    /**
+     * Decode url, along with dots and hyphens
+     * @param string $input
+     * @return string
+     */
+    public static function urldecode(string $input): string
+    {
+        $str = urldecode($input);
+        $str = str_replace('%2E', '.', $str);
+        $str = str_replace('%2D', '-', $str);
+        return $str;
+    }
+}
