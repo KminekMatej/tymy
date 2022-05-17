9498f2b953a699ea6d02f8b5aeb083eec8141905
Matěj Kmínek <matej.kminek@attendees.eu>
2022-05-17 23:20:16 +0200

    feat: Make react() js function universal

--- www/public/js/Discussion/Discussion.js
+++ www/public/js/Discussion/Discussion.js
@@ -1,15 +1,54 @@
 $(function () {
+    togglePopovers();
+});
+
+function togglePopovers() {
     $(".emoji-toggler").each(function () {
+        var rowId = $(this).closest("DIV.post").attr("id").replace("row-","");
         $(this).popover({
-            content: function () {
-                // Get the content from the hidden sibling.
-                return $('#emoji-card').html();
-            },
+            content: $('#emoji-area-' + rowId).html(),
             html: true,
-            placement: 'auto',
+            placement: 'top',
+            container: '#row-' + rowId + " .emoji-toggler",
+            trigger: 'click',
+            whiteList: {
+                // Global attributes allowed on any supplied element below.
+                '*': ['class', 'dir', 'id', 'lang', 'role'],
+                a: ['target', 'href', 'title', 'rel', 'data-toggle', 'onclick'],
+                area: [],
+                b: [],
+                button: ['onclick'],
+                br: [],
+                col: [],
+                code: [],
+                div: ['aria-labelledby'],
+                em: [],
+                hr: [],
+                h1: [],
+                h2: [],
+                h3: [],
+                h4: [],
+                h5: [],
+                h6: [],
+                i: [],
+                img: ['src', 'srcset', 'alt', 'title', 'width', 'height'],
+                li: [],
+                ol: [],
+                p: [],
+                pre: [],
+                s: [],
+                svg: ['onclick'],
+                small: [],
+                span: ['onclick'],
+                sub: [],
+                sup: [],
+                strong: [],
+                u: [],
+                ul: []
+            }
         });
     });
-});
+}
 
 function addPost(url) {
     $("DIV.addPost BUTTON").prop("disabled", true);
@@ -81,73 +120,77 @@ function stickPost(url, postId, sticky) {
     });
 }
 
-function react(postId, reaction, remove) {
-    var badges = $("DIV#row-" + postId + " DIV.reaction-bar .badge");
-    var badgeExists = false;
+function react(el) {
+    var reaction = $(el).find(".emoji").html() || $(el).html();
+    var post = $(el).closest("DIV.post");
 
-    badges.each(function () {
-        var emojiEl = $(this).find(".emoji");
-        var countEl = $(this).find(".emoji-count");
-        if (emojiEl.html() == reaction) {
-            badgeExists = true;
-            //either increase or decrease badge count
-            var currentCount = parseInt(countEl.html());
-            if (remove) { //removal my reaction
-                currentCount--;
-                $(this).removeClass("border-primary");
-            } else {
-                currentCount++;
-                $(this).addClass("border-primary");
-            }
+    //detect if this is removal
+    var remove = false;
+    var reactionBar = post.find("DIV.reaction-bar");
+    var myBadges = reactionBar.find(".badge.border-primary");
+    var badge = null;
 
-            countEl.html(currentCount);
-
-            if (currentCount == 1) { //hide badge count
-                countEl.hide();
-            } else if (currentCount > 1) {// display badge count
-                countEl.show();
-            } else if (currentCount == 0) { //hide whole badge
-                $(this).remove();
-            }
-        }
-    });
-
-    if (!badgeExists) { //this badge doesnt exists yet - create it
-        var likeBtnHref = $("DIV#row-" + postId + " .like-btn").attr("href") + "&remove=1"; //copy likeBtnHref - but that doesnt contain remove parameter yet (since it hasnt been reloaded yet - so forcefully append the remove param into query)
-
-        $("DIV#row-" + postId + " DIV.reaction-bar").append("<a href=\"" + likeBtnHref + "\" \n\
-            class=\"badge badge-light ajax my-1 mr-1 border border-primary\" \n\
-            onclick=\"react(" + postId + ", '" + reaction + "', 1)\">\n\
-            <span class=\"emoji\">" + reaction + "</span>\n\
-            <span class=\"emoji-count\" style=\"display: none\">1</span></a>");
-    }
-
-    reloadReactButton(postId);
-
-    return true;
-}
-
-function reloadReactButton(postId) {
-    var badges = $("DIV#row-" + postId + " DIV.reaction-bar .badge");
-    var likeBtn = $("DIV#row-" + postId + " .like-btn");
-    var reactionExists = false;
-
-    badges.each(function () {
-        if ($(this).hasClass("border-primary")) {//this button contains my reaction
-            var emojiEl = $(this).find(".emoji");
-            likeBtn.html(emojiEl.html()); //add reaction icon
-            likeBtn.addClass("border-primary"); //add reaction icon
-            likeBtn.attr("href", $(this).attr("href"));
-            likeBtn.attr("onclick", $(this).attr("onclick"));//copy the same href and onclick from this button - as it should do exactly the same thing
-            reactionExists = true;
+    myBadges.each(function () {
+        if ($(this).find(".emoji").html() == reaction) {
+            remove = true;
+            badge = $(this);
             return false; //break jquery .each loop
         }
     });
 
-    if (!reactionExists) { //restore default values if there is no reaction found
-        likeBtn.html('👍'); //add reaction icon
-        likeBtn.removeClass("border-primary"); //add reaction icon
-        likeBtn.attr("href", likeBtn.attr("data-default-href"));
-        likeBtn.attr("onclick", likeBtn.attr("data-default-onclick"));//reset default href and onclick parameters
+    var badges = reactionBar.find(".badge");
+
+    if (!badge) {   //badge has not been found in my badges, try to detect it in all badges
+        badges.each(function () {
+            if ($(this).find(".emoji").html() == reaction) {
+                badge = $(this);
+            }
+        });
+    }
+
+    if (badge) {
+        var countEl = badge.find(".emoji-count");
+
+        //either increase or decrease badge count
+        var currentCount = parseInt(countEl.html());
+        if (remove) { //removal my reaction
+            currentCount--;
+            badge.removeClass("border-primary");
+        } else {
+            currentCount++;
+            badge.addClass("border-primary");
+        }
+        countEl.html(currentCount);
+        if (currentCount == 1) { //hide badge count
+            countEl.hide();
+        } else if (currentCount > 1) {// display badge count
+            countEl.show();
+        } else if (currentCount == 0) { //hide whole badge
+            badge.remove();
+        }
+    } else { //this badge doesnt exists yet - create it
+        reactionBar.append("<span \n\
+            class=\"badge badge-light my-1 mr-1 border border-primary\" \n\
+            onclick=\"react(this)\">\n\
+            <span class=\"emoji\">" + reaction + "</span>\n\
+            <span class=\"emoji-count\" style=\"display: none\">1</span>\n\
+        </span>");
+    }
+
+    //store the reaction on server
+    $.nette.ajax({
+        url: reactionBar.attr('data-reaction-url') + '&reaction=' + encodeURIComponent(reaction) + '&remove=' + (remove ? 1 : 0),
+    });
+}
+
+function toggleEmojis(el) {
+    event.stopPropagation();
+    var emojiBody = $(el).closest(".emoji-card").find(".emoji-body");
+    if(emojiBody.hasClass("fade")){
+        emojiBody.removeClass("d-none");
+        emojiBody.removeClass("fade");
+    } else {
+        emojiBody.addClass("fade");
+        emojiBody.addClass("d-none");
     }
 }
