bad76dd8795b96870bd4690e14fb5d2e45910327
Matěj Kmínek <matej.kminek@attendees.eu>
2022-08-07 09:28:31 +0200

    feat: Add possibility to create new option line

--- www/public/js/Setting/poll.js
+++ www/public/js/Setting/poll.js
@@ -1,18 +1,21 @@
-function duplicateLastRow(){
-    var lastRow = $("DIV.settings DIV[data-option]:last");
+function duplicateLastOptionRow() {
+    var lastRow = $("DIV.settings DIV.option-row:last");
+    var captionSelector = "INPUT";
+    var valueSelector = "SELECT";
+    var lastCaptionInput = lastRow.find(captionSelector);
+    var lastRowId = parseInt(lastCaptionInput.attr("name").replace("option_caption_", ""));
+    var newRowId = lastRowId + 1;
     var newRow = lastRow.clone();
-    newRow.insertAfter(lastRow);
-}
+    newRow.find(captionSelector).attr({
+        name: "option_caption_" + newRowId,
+        id: "from-pollForm-option_caption_" + newRowId,
+        'data-lfv-message-id': "frm-pollForm-option_caption_" + newRowId + "_message",
+    }).next("span").attr("id", "frm-pollForm-option_caption_" + newRowId + "_message");
+    newRow.find(valueSelector).attr({
+        name: "option_type_" + newRowId,
+        id: "from-pollForm-option_type_" + newRowId,
+        'data-lfv-message-id': "frm-pollForm-option_type_" + newRowId + "_message",
+    }).next("span").attr("id", "frm-pollForm-option_type_" + newRowId + "_message");
 
-function createNewRow(){
-    duplicateLastRow();
-    var lastRow = $("DIV.settings DIV[data-option]:last");
-    lastRow.attr("data-binder-id",-1);
-    
-    lastRow.data("data-binder", new Binder({
-            area: lastRow,
-        }));
-    lastRow.data("data-binder").changeSaveButtonClass(true);
-    lastRow.find("[data-value]").attr("data-value","null");
-    lastRow.find("[data-value]").attr("data-value","null");
+    newRow.insertAfter(lastRow);
 }
