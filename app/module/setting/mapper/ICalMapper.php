008f806297b7c65bc3a4a242bb9805a527fb2fec
Matěj Kmínek <matej.kminek@attendees.eu>
2022-09-11 22:12:37 +0200

    feat: Do not set changeable to be able to create it

--- app/module/setting/mapper/ICalMapper.php
+++ app/module/setting/mapper/ICalMapper.php
@@ -19,9 +19,8 @@ class ICalMapper extends BaseMapper
             Field::int()->withPropertyAndColumn("id", false, false),
             Field::datetime()->withPropertyAndColumn("created", false, false),
             Field::int()->withColumn("created_user_id")->setProperty("createdUserId"),
-            
             Field::int()->withColumn("user_id", true)->setProperty("userId"),
-            Field::string()->withPropertyAndColumn("hash")->setChangeable(false),
+            Field::string()->withPropertyAndColumn("hash"),
             Field::int()->withColumn("enabled")->setProperty("enabled"),
         ];
     }
