97f4662799e0c30f91d5dc0fb1486b349485cfc4
Matěj Kmínek <matej.kminek@attendees.eu>
2022-09-11 14:01:48 +0200

    feat: Remove operating with user data in session - rather load user object during every request

--- app/module/sign/presenter/front/InPresenter.php
+++ app/module/sign/presenter/front/InPresenter.php
@@ -40,8 +40,8 @@ class InPresenter extends BasePresenter
                         break;
                 }
             }
-            if (!is_null($this->user->getIdentity())) {
-                Debugger::log($this->user->getIdentity()->data["callName"] . "@" . $this->team->getSysName() . " logged in");
+            if ($this->user->isLoggedIn()) {
+                Debugger::log($this->tymyUser->getCallName() . "@" . $this->team->getSysName() . " logged in");
             }
             $this->redirect(':Core:Default:');
         });
@@ -64,8 +64,8 @@ class InPresenter extends BasePresenter
                         break;
                 }
             }
-            if (!is_null($this->user->getIdentity())) {
-                Debugger::log($this->user->getIdentity()->data["callName"] . "@" . $this->team->getSysName() . " logged in using transfer key");
+            if ($this->user->isLoggedIn()) {
+                Debugger::log($this->tymyUser->getCallName() . "@" . $this->team->getSysName() . " logged in using transfer key");
             }
             $this->redirect(':Core:Default:');
         }
