ae869d4dbe152c477a1fd21e497d5a4b74c618f6
Matěj Kmínek <matej.kminek@attendees.eu>
2022-09-16 21:45:38 +0200

    feat: Add specific route to match old links to calendar

--- app/module/user/router/Router.php
+++ app/module/user/router/Router.php
@@ -2,8 +2,8 @@
 
 namespace Tymy\Module\User\Router;
 
-use Tymy\Module\Core\Router\RouteList;
 use Tymy\Module\Core\Interfaces\RouterInterface;
+use Tymy\Module\Core\Router\RouteList;
 
 /**
  * Description of Router
@@ -12,22 +12,29 @@ use Tymy\Module\Core\Interfaces\RouterInterface;
  */
 class Router implements RouterInterface
 {
+
     public function addRoutes(RouteList &$router): void
     {
-        $router->addApiRoute('user[s]/status[/<status>]', array(
-                    'module' => 'User',
-                    'presenter' => 'Status',
-                    'action' => 'default',
-                ))
-                ->addApiRoute('pwd<action reset|lost>[/<code>]', array(
-                    'module' => 'User',
-                    'presenter' => 'Pwd',
-                    'action' => 'default',
-                ))
-                ->addApiRoute('live', array(
-                    'module' => 'User',
-                    'presenter' => 'Live',
-                    'action' => 'default',
-                ));
+        //add route to be able to server ical calendars on the same url like in previous version
+        $router->addRoute("index.php ? page=calexp & user_id=<resource> & hash=<hash>", [
+                'module' => 'User',
+                'presenter' => 'Detail',
+                'action' => 'calendar'
+                ], $router::ONE_WAY)
+            ->addApiRoute('user[s]/status[/<status>]', [
+                'module' => 'User',
+                'presenter' => 'Status',
+                'action' => 'default',
+            ])
+            ->addApiRoute('pwd<action reset|lost>[/<code>]', [
+                'module' => 'User',
+                'presenter' => 'Pwd',
+                'action' => 'default',
+            ])
+            ->addApiRoute('live', [
+                'module' => 'User',
+                'presenter' => 'Live',
+                'action' => 'default',
+        ]);
     }
 }
