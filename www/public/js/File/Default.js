6181fdcd37dc08306262c314e1df1ccde1784ccd
Matěj Kmínek <matej.kminek@attendees.eu>
2022-09-16 22:35:12 +0200

    feat: Add sorting file table

--- www/public/js/File/Default.js
+++ www/public/js/File/Default.js
@@ -15,4 +15,22 @@ $(document).ready(function () {
         }
     });
 
+    $.tablesorter.addParser({
+        id: "czechDT",
+        is: function (s) {
+            return /\d{1,2}\.\d{1,2}\.\d{1,4} \d{1,2}:\d{1,2}:\d{1,2}/.test(s);
+        },
+        format: function (s) {
+            s = s.replace(/\./g, " ");
+            s = s.replace(/:/g, " ");
+            s = s.replace(/\./g, " ");
+            s = s.split(" ");
+            return $.tablesorter.formatFloat(new Date(s[2], s[1] - 1, s[0], s[3], s[4], s[5]).getTime());
+        },
+        type: "numeric"
+    });
+
+    $(".tablesorter").tablesorter({
+        theme: 'bootstrap_4',
+    });
 });
