02a6c7aad87ce85d8ae4c85d86fa80c552cfdfa5
Matěj Kmínek <matej.kminek@attendees.eu>
2022-09-27 08:35:16 +0200

    feat: Created basic invitation presenter and add manager into DIC

--- app/module/team/presenter/front/InvitationPresenter.php
+++ app/module/team/presenter/front/InvitationPresenter.php
@@ -2,7 +2,10 @@
 
 namespace Tymy\Module\Team\Presenter\Front;
 
+use Nette\Application\UI\Form;
+use stdClass;
 use Tymy\Module\Core\Presenter\Front\SecuredPresenter;
+use Tymy\Module\User\Manager\InvitationManager;
 
 /**
  * Description of InvitationPresenter
@@ -11,8 +14,41 @@ use Tymy\Module\Core\Presenter\Front\SecuredPresenter;
  */
 class InvitationPresenter extends SecuredPresenter
 {
+    /** @inject */
+    public InvitationManager $invitationManager;
+
+    public function beforeRender()
+    {
+        parent::beforeRender();
+
+        $this->addBreadcrumb($this->translator->translate("team.team", 1), $this->link(":Team:Default:"));
+        $this->addBreadcrumb($this->translator->translate("team.invitation", 2), $this->link(":Team:Invitation:"));
+    }
+
     public function renderDefault()
     {
-        
+        $this->template->invitations = $this->invitationManager->getList();
+    }
+
+    public function createComponentInvitationForm()
+    {
+        $form = new Form();
+
+        $form->addText("firstName", $this->translator->translate("team.firstName"));
+        $form->addText("lastName", $this->translator->translate("team.lastName"));
+        $form->addText("email", $this->translator->translate("team.email"));
+
+        $form->addSubmit("save");
+
+        $form->onSuccess[] = [$this, ["invitationFormSuccess"]];
+
+        return $form;
+    }
+
+    public function invitationFormSuccess(Form $form, stdClass $values)
+    {
+        $this->invitationManager->create((array) $values);
+
+        $this->flashMessage($this->translator->translate("team.alerts.invitationCreated"), "success");
     }
 }
