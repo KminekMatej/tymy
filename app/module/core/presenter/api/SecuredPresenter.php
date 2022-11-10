d39e37d11f563a734bad646d2080ea34f6ceeb7e
Matěj Kmínek <matej.kminek@attendees.eu>
2022-11-10 16:11:01 +0100

    feat: Add ability to access api using login/password in query or in header

--- app/module/core/presenter/api/SecuredPresenter.php
+++ app/module/core/presenter/api/SecuredPresenter.php
@@ -2,6 +2,9 @@
 
 namespace Tymy\Module\Core\Presenter\Api;
 
+use Nette\Security\AuthenticationException;
+use Tracy\Debugger;
+use Tymy\Module\Core\Manager\BaseManager;
 use Tymy\Module\User\Model\User;
 
 /**
@@ -12,6 +15,8 @@ use Tymy\Module\User\Model\User;
 class SecuredPresenter extends BasePresenter
 {
     private const TSID = "TSID";
+    private const LOGIN_PARAM = "login";
+    private const PASSWORD_PARAM = "password";
 
     public function startup(): void
     {
@@ -24,6 +29,26 @@ class SecuredPresenter extends BasePresenter
             session_id($tsid);
             $this->session->start();
             $this->user->refreshStorage();
+        }
+
+        if (!$this->user->isLoggedIn()) {
+            $username = $this->getParameter(self::LOGIN_PARAM) ?: $this->httpRequest->getHeader(self::LOGIN_PARAM);
+            $password = $this->getParameter(self::PASSWORD_PARAM) ?: $this->httpRequest->getHeader(self::PASSWORD_PARAM);
+
+            if (!empty($username) && !empty($password)) {
+                try {
+                    $this->user->setExpiration(null);
+                    $this->user->login($username, $password);
+                    BaseManager::logg($this->team, "$username API direct access");
+                } catch (AuthenticationException $exc) {
+                    $this->responder->E401_UNAUTHORIZED("Not logged in");
+                }
+            }
+
+            if ($this->user->isLoggedIn()) {
+                $this->initUser();
+                Debugger::log($this->tymyUser->getCallName() . "@" . $this->team->getSysName() . " directly accessed");
+            }
         }
 
         if ($this->user === null || !$this->user->isLoggedIn()) {
