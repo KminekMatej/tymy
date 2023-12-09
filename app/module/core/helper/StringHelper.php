67f36c33842e6e0859cd9edcebc56e1243ff9647
Matěj Kmínek <m@kminet.eu>
2023-12-09 10:52:25 +0100

    feat: Add toCalemCase method

--- app/module/core/helper/StringHelper.php
+++ app/module/core/helper/StringHelper.php
@@ -28,4 +28,22 @@ class StringHelper
         $str = str_replace('%2E', '.', $str);
         return str_replace('%2D', '-', $str);
     }
+
+    /**
+     * Convert string written in dashes and underscores to camelCase
+     *
+     * @param string $string
+     * @param bool $capitalizeFirstCharacter
+     * @return string
+     */
+    public static function toCamelCase(string $string, bool $capitalizeFirstCharacter = false): string
+    {
+        $str = str_replace(' ', '', ucwords(str_replace(['-', '_'], ' ', $string)));
+
+        if (!$capitalizeFirstCharacter) {
+            $str[0] = strtolower($str[0]);
+        }
+
+        return $str;
+    }
 }
